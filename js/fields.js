56315366edd2b9d7d279fe1b0ecd6365ef710c39
Siyan Panayotov <contact@siyanpanayotov.com>
2012-09-24 12:32:13 +0000

    carbon-fields - revamp fields javascript (wip, done repeater)

--- js/fields.js
+++ js/fields.js
@@ -1,5 +1,158 @@
 jQuery(function($) {
 
+	function init(context) {
+		var fields;
+
+		if ( !context ) {
+			context = $('body');
+		};
+
+		fields = $('.eecf-field:not(.eecf-field-skip)', context);
+
+		fields.each(function() {
+			var th = $(this),
+				type = th.data('type'),
+				field;
+
+			if ( typeof eecf_field[type] == 'undefined' ) {
+				return;
+			};
+
+			try {
+				field = eecf_field(th);
+
+				if ( typeof eecf_field[type] != 'undefined' ) {
+					eecf_field[type](th, field);
+				};
+			} catch (e) {
+				throw e;
+			}
+		});
+	}
+
+	function eecf_field(node) {
+		var field = {};
+
+		if ( node.data('eecf_field') ) {
+			$.error('Field already parsed');
+		};
+
+		node.data('eecf_field', field);
+		field.node = node;
+		field.type = node.data('type')
+
+		return field;
+	}
+
+	/* File and Image */
+	eecf_field.File = function(element, field_obj) {
+		element.find('.button-primary').click(function() {
+			window.ecf_active_field = element;
+			tb_show('','media-upload.php?TB_iframe=true');
+		});
+	}
+
+	eecf_field.Image = function(element, field_obj) {
+		element.find('.button-primary').click(function() {
+			window.ecf_active_field = element;
+			tb_show('','media-upload.php?type=image&amp;TB_iframe=true');
+		});
+	}
+
+	/* Repeater Field */
+	eecf_field.Repeater = function(element, field_obj) {
+		// prepare object
+		field_obj.btn_add = element.find('a[data-action=add]');
+		field_obj.num_rows = element.find('.eecf-repeater-row').length;
+		field_obj.min_rows = element.children('.eecf-container').data('min-values');
+		field_obj.max_rows = element.children('.eecf-container').data('max-values');
+
+		field_obj.name = element.data('name');
+
+		// init
+		while( field_obj.num_rows < field_obj.min_rows ) {
+			repeater_add_row(field_obj);
+		}
+
+		if ( field_obj.max_rows > 0 && field_obj.num_rows >= field_obj.max_rows ) {
+			field_obj.btn_add.hide();
+		};
+
+		// Hook events
+
+		field_obj.btn_add.click(function() {
+			repeater_add_row(field_obj);
+			return false;
+		});
+
+		field_obj.node.find('a[data-action=remove]').live('click', function() {
+			repeater_remove_row(field_obj, $(this).closest('.eecf-repeater-row'));
+			return false;
+		});
+	}
+
+	function repeater_add_row(field) {
+		var sample_row, new_row;
+
+		if ( field.max_rows > 0 && field.max_rows <= field.num_rows ) {
+			alert('Maximum number of rows reached (' + field.num_rows + ')');
+			return;
+		};
+
+		sample_row = field.node.find('.eecf-repeater-preview');
+		new_row = sample_row.clone();
+
+		field.num_rows++;
+
+		new_row.find('.eecf-field-skip').removeClass('eecf-field-skip');
+
+		new_row.find('input[name*="__ei__"]').each(function() {
+			var input = $(this);
+			input.attr('name', input.attr('name').replace(/\[__ei__\]/, '[' + field.num_rows + ']'));
+		});
+
+		new_row.removeClass('eecf-repeater-preview').addClass('eecf-repeater-row').insertBefore(sample_row);
+		init(new_row);
+
+		if ( field.max_rows > 0 && field.num_rows == field.max_rows ) {
+			field.btn_add.hide();
+		};
+	}
+
+	function repeater_remove_row (field, row) {
+		row.remove();
+		repeater_on_update_rows(field);
+
+		if ( field.min_rows > field.num_rows ) {
+			setTimeout(function() {
+				repeater_add_row(field);
+			}, 0);
+		};
+
+		if ( field.max_rows > 0 && field.num_rows <= field.max_rows ) {
+			field.btn_add.show();
+		};
+	};
+
+	function repeater_on_update_rows (field) {
+		var rows = field.node.find('.eecf-repeater-row'),
+			index = 0;
+
+		field.num_rows = rows.length;
+
+		rows.each(function() {
+			var row = $(this);
+			index ++;
+
+			row.find('input[name^="' + field.name + '"]').each(function() {
+				var input = $(this);
+				input.attr('name', input.attr('name').replace(/\[\d+\]/, '[' + index + ']'));
+			});
+		});
+	}
+
+	// ---------------------
+
 	function EECF_Field (node) {
 		if ( node.data('eecf_field') ) {
 			$.error('Field already parsed');
@@ -399,7 +552,8 @@ jQuery(function($) {
 	});
 
 
-	EECF_Field.init();
+	// EECF_Field.init();
+	init();
 
 	window.EECF_Field = EECF_Field;
 });
