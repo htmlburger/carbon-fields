e50651cb250b4036f79b965955dba5f1aaaf6e95
Siyan Panayotov <contact@siyanpanayotov.com>
2012-09-26 13:58:44 +0000

    carbon-fields - add header and footer scripts

--- Carbon_Field.php
+++ Carbon_Field.php
@@ -591,7 +591,7 @@ class Carbon_Field_Radio extends Carbon_Field {
 
 			echo '/>' . htmlentities($value, ENT_COMPAT, 'UTF-8') . '</label></li>';
 		}
-		
+
     	echo '</ul>';
 	}
 }
@@ -619,6 +619,40 @@ class Carbon_Field_Checkbox extends Carbon_Field {
 
 	function get_label() {
 		return '';
+	}
+}
+
+class Carbon_Field_Header_Scripts extends Carbon_Field_Textarea {
+	function init() {
+		$this->help_text('If you need to add scripts to your header, you should enter them here.');
+
+		add_action('wp_head', array($this, 'print_scripts'));
+		Carbon_Field::init();
+	}
+	
+	function print_scripts() {
+		if ( !$this->store || !is_a($this->store, 'Carbon_DataStore_ThemeOptions') ) {
+			return;
+		}
+
+	    echo get_option($this->name);
+	}
+}
+
+class Carbon_Field_Footer_Scripts extends Carbon_Field_Textarea {
+	function init() {
+		$this->help_text('If you need to add scripts to your footer (like Google Analytics tracking code), you should enter them in this box.');
+
+		add_action('wp_footer', array($this, 'print_scripts'));
+		Carbon_Field::init();
+	}
+	
+	function print_scripts() {
+		if ( !$this->store || !is_a($this->store, 'Carbon_DataStore_ThemeOptions') ) {
+			return;
+		}
+		
+	    echo get_option($this->name);
 	}
 }
 
