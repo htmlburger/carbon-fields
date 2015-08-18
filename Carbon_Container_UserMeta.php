3e76fd71f278a4d5ffe6e04b8e2872e80839bae4
Marin Atanasov <tyxla@abv.bg>
2015-08-18 14:48:07 +0000

    Carbon_Container_UserMeta: Fixing an issue with multisite roles

--- Carbon_Container_UserMeta.php
+++ Carbon_Container_UserMeta.php
@@ -65,7 +65,8 @@ class Carbon_Container_UserMeta extends Carbon_Container {
 		// Check user role
 		if ( !empty($this->settings['show_on']['role']) ) {
 			$allowed_roles = (array) $this->settings['show_on']['role'];
-			if ( !in_array($user->roles[0], $allowed_roles) ) {
+			$profile_role = array_shift($user->roles);
+			if ( !in_array($profile_role, $allowed_roles) ) {
 				$valid = false;
 			}
 		}
