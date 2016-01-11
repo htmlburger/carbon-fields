10757f3abbc592ffab030951943691c3b82a01c4
Marin Atanasov <tyxla@abv.bg>
2016-01-11 16:11:02 +0200

    Post_Meta_Container: minor fix in nonce checks

--- core/Container/Post_Meta_Container.php
+++ core/Container/Post_Meta_Container.php
@@ -155,7 +155,7 @@ class Post_Meta_Container extends Container {
 	public function is_valid_save( $post_id = 0 ) {
 		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
 			return false;
-		} else if ( ! wp_verify_nonce( $_REQUEST[ $this->get_nonce_name() ], $this->get_nonce_name() ) ) {
+		} else if ( ! isset( $_REQUEST[ $this->get_nonce_name() ] ) || ! wp_verify_nonce( $_REQUEST[ $this->get_nonce_name() ], $this->get_nonce_name() ) ) {
 			return false;
 		} else if ( $post_id < 1 ) {
 			return false;
