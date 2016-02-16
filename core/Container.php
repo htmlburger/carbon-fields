75cf33f358f7d47a1cb5fbf78ea6d9b36c980581
Marin Atanasov <tyxla@abv.bg>
2016-02-16 10:45:36 +0200

    add proxy Container for allowing shorter 'use' statements

---
+++ core/Container.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace Carbon_Fields;
+
+use Carbon_Fields\Container\Container as Abstract_Container;
+
+/**
+ * Container proxy factory class.
+ * Used for shorter namespace access when creating a container.
+ **/
+class Container {
+	/**
+	 * A proxy for the abstract field factory method.
+	 *
+	 * @see Carbon_Fields\Container\Container::factory()
+	 **/
+	public static function factory( $type, $name, $label = null ) {
+		return Abstract_Container::factory( $type, $name, $label );
+	}
+
+	/**
+	 * An alias of factory().
+	 *
+	 * @see Container::factory()
+	 **/
+	public static function make( $type, $name, $label = null ) {
+		return self::factory( $type, $name, $label );
+	}
+}
