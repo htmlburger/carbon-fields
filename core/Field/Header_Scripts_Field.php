1a45bc6cd28c2a7a48f5b108b3d985744196b025
Marin Atanasov <tyxla@abv.bg>
2016-02-29 12:53:06 +0200

    abstracted common functionality of Header_Scripts_Field and Footer_Scripts_Field to reduce duplication and allow easier reuse

--- core/Field/Header_Scripts_Field.php
+++ core/Field/Header_Scripts_Field.php
@@ -6,26 +6,18 @@ namespace Carbon_Fields\Field;
  * Header scripts field class.
  * Intended only for use in theme options container.
  */
-class Header_Scripts_Field extends Textarea_Field {
+class Header_Scripts_Field extends Scripts_Field {
 	/**
-	 * Initialization actions
+	 * Default help text to be displayed for this type of field.
 	 */
-	public function init() {
-		$this->help_text( __( 'If you need to add scripts to your header, you should enter them here.', 'carbon_fields' ) );
-
-		add_action( 'wp_head', array( $this, 'print_scripts' ) );
-
-		parent::init();
+	public function get_default_help_text() {
+		return __( 'If you need to add scripts to your header, you should enter them here.', 'carbon_fields' );
 	}
 
 	/**
-	 * Display the field value in the front-end header.
+	 * Action name to be hooked on.
 	 */
-	public function print_scripts() {
-		if ( ! $this->store || ! is_a( $this->store, 'Carbon_Fields\\Datastore\\Theme_Options_Datastore' ) ) {
-			return;
-		}
-
-		echo get_option( $this->name );
+	public function get_hook_name() {
+		return 'wp_head';
 	}
 }
---
+++ core/Field/Scripts_Field.php
@@ -0,0 +1,41 @@
+<?php 
+
+namespace Carbon_Fields\Field;
+
+/**
+ * Abstract scripts field class.
+ * Intended only for use in theme options container.
+ */
+abstract class Scripts_Field extends Textarea_Field {
+	/**
+	 * Initialization actions
+	 */
+	public function init() {
+		$this->help_text( $this->get_default_help_text() );
+
+		add_action( $this->get_hook_name(), array( $this, 'print_scripts' ) );
+
+		parent::init();
+	}
+
+	/**
+	 * Display the field value in the front-end header.
+	 */
+	public function print_scripts() {
+		if ( ! $this->store || ! is_a( $this->store, 'Carbon_Fields\\Datastore\\Theme_Options_Datastore' ) ) {
+			return;
+		}
+
+		echo get_option( $this->name );
+	}
+
+	/**
+	 * Default help text to be displayed for this type of field.
+	 */
+	abstract public function get_default_help_text();
+
+	/**
+	 * Action name to be hooked on.
+	 */
+	abstract public function get_hook_name();
+}
