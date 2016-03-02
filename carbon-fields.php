f2e69e3067e8408c9941d5e8c77d47fb3521b7f3
Nicolas Lemoine <nico.lemoine@gmail.com>
2016-03-02 14:21:40 +0100

    As of PHP 5.3, you can use __DIR__ magic constant instead of dirname( __FILE__ )

--- carbon-fields.php
+++ carbon-fields.php
@@ -5,7 +5,7 @@ use Carbon_Fields\Helper\Helper;
 
 # Define root directory
 if ( ! defined( __NAMESPACE__ . '\DIR' ) ) {
-	define( __NAMESPACE__ . '\DIR', dirname( __FILE__ ) );
+	define( __NAMESPACE__ . '\DIR', __DIR__ );
 }
 
 # Define root URL
@@ -35,7 +35,7 @@ if ( ! defined( __NAMESPACE__ . '\URL' ) ) {
 	define( __NAMESPACE__ . '\URL', \untrailingslashit( $url ) );
 }
 
-# Initialize helper 
+# Initialize helper
 global $carbon_fields_helper;
 if ( ! isset( $carbon_fields_helper ) ) {
 	$carbon_fields_helper = new Helper();
