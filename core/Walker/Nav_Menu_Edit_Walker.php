42b10147c2ca3a473985fd2e833f1c24a9d50112
Marin Atanasov <tyxla@abv.bg>
2016-03-10 11:25:05 +0200

    wrap Nav_Menu_Edit_Walker around Walker_Nav_Menu_Edit to keep the walker DRY

--- core/Walker/Nav_Menu_Edit_Walker.php
+++ core/Walker/Nav_Menu_Edit_Walker.php
@@ -5,26 +5,9 @@ namespace Carbon_Fields\Walker;
 /**
  * Walker for the administration nav menu editing.
  *
- * @uses Walker_Nav_Menu
+ * @uses Walker_Nav_Menu_Edit
  */
-class Nav_Menu_Edit_Walker extends \Walker_Nav_Menu {
-	/**
-	 * Starts the list before the elements are added.
-	 *
-	 * @param string $output Passed by reference. Used to append additional content.
-	 * @param int    $depth  Depth of menu item. Used for padding.
-	 * @param array  $args   An array of arguments. @see wp_nav_menu()
-	 */
-	public function start_lvl( &$output, $depth = 0, $args = array() ) {}
-
-	/**
-	 * Ends the list of after the elements are added.
-	 *
-	 * @param string $output Passed by reference. Used to append additional content.
-	 * @param int    $depth  Depth of menu item. Used for padding.
-	 * @param array  $args   An array of arguments. @see wp_nav_menu()
-	 */
-	public function end_lvl( &$output, $depth = 0, $args = array() ) {}
+class Nav_Menu_Edit_Walker extends \Walker_Nav_Menu_Edit {
 
 	/**
 	 * Start the element output.
@@ -36,197 +19,16 @@ class Nav_Menu_Edit_Walker extends \Walker_Nav_Menu {
 	 * @param int    $id     Current item ID.
 	 */
 	public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
-		global $_wp_nav_menu_max_depth;
-		$_wp_nav_menu_max_depth = $depth > $_wp_nav_menu_max_depth ? $depth : $_wp_nav_menu_max_depth;
+		parent::start_el( $output, $item, $depth, $args, $id );
+
+		$flag = '<!--CarbonFields-->';
 
 		ob_start();
-		$item_id = esc_attr( $item->ID );
-		$removed_args = array(
-			'action',
-			'customlink-tab',
-			'edit-menu-item',
-			'menu-item',
-			'page-tab',
-			'_wpnonce',
-		);
+		do_action( 'crb_print_carbon_container_nav_menu_fields_html', $item, $output, $depth, $args, $id );
+		echo $flag;
+		$fields = ob_get_clean();
 
-		$original_title = '';
-		if ( 'taxonomy' == $item->type ) {
-			$original_title = get_term_field( 'name', $item->object_id, $item->object, 'raw' );
-			if ( is_wp_error( $original_title ) ) {
-				$original_title = false;
-			}
-		} elseif ( 'post_type' == $item->type ) {
-			$original_object = get_post( $item->object_id );
-			$original_title = get_the_title( $original_object->ID );
-		}
-
-		$classes = array(
-			'menu-item menu-item-depth-' . $depth,
-			'menu-item-' . esc_attr( $item->object ),
-			'menu-item-edit-' . ( ( isset( $_GET['edit-menu-item'] ) && $item_id == $_GET['edit-menu-item'] ) ? 'active' : 'inactive' ),
-		);
-
-		$title = $item->title;
-
-		if ( ! empty( $item->_invalid ) ) {
-			$classes[] = 'menu-item-invalid';
-			/* translators: %s: title of menu item which is invalid */
-			$title = sprintf( __( '%s (Invalid)', 'carbon_fields' ), $item->title );
-		} elseif ( isset( $item->post_status ) && 'draft' == $item->post_status ) {
-			$classes[] = 'pending';
-			/* translators: %s: title of menu item in draft status */
-			$title = sprintf( __( '%s (Pending)', 'carbon_fields' ), $item->title );
-		}
-
-		$title = ( ! isset( $item->label ) || '' == $item->label ) ? $title : $item->label;
-
-		$submenu_text = '';
-		if ( 0 == $depth ) {
-			$submenu_text = 'style="display: none;"';
-		}
-		?>
-		<li id="menu-item-<?php echo esc_attr( $item_id ); ?>" class="<?php echo implode( ' ', $classes ); ?>">
-			<div class="menu-item-bar">
-				<div class="menu-item-handle">
-					<span class="item-title"><span class="menu-item-title"><?php echo esc_html( $title ); ?></span> <span class="is-submenu" <?php echo $submenu_text; ?>><?php _e( 'sub item', 'carbon_fields' ); ?></span></span>
-					<span class="item-controls">
-						<span class="item-type"><?php echo esc_html( $item->type_label ); ?></span>
-						<span class="item-order hide-if-js">
-							<a href="<?php
-								echo wp_nonce_url(
-									add_query_arg(
-										array(
-											'action' => 'move-up-menu-item',
-											'menu-item' => $item_id,
-										),
-										remove_query_arg( $removed_args, admin_url( 'nav-menus.php' ) )
-									),
-									'move-menu_item'
-								);
-							?>" class="item-move-up"><abbr title="<?php esc_attr_e( 'Move up', 'carbon_fields' ); ?>">&#8593;</abbr></a>
-							|
-							<a href="<?php
-								echo wp_nonce_url(
-									add_query_arg(
-										array(
-											'action' => 'move-down-menu-item',
-											'menu-item' => $item_id,
-										),
-										remove_query_arg( $removed_args, admin_url( 'nav-menus.php' ) )
-									),
-									'move-menu_item'
-								);
-							?>" class="item-move-down"><abbr title="<?php esc_attr_e( 'Move down', 'carbon_fields' ); ?>">&#8595;</abbr></a>
-						</span>
-						<a class="item-edit" id="edit-<?php echo esc_attr( $item_id ); ?>" title="<?php esc_attr_e( 'Edit Menu Item', 'carbon_fields' ); ?>" href="<?php
-							echo ( isset( $_GET['edit-menu-item'] ) && $item_id == $_GET['edit-menu-item'] ) ? admin_url( 'nav-menus.php' ) : add_query_arg( 'edit-menu-item', $item_id, remove_query_arg( $removed_args, admin_url( 'nav-menus.php#menu-item-settings-' . $item_id ) ) );
-						?>"><?php _e( 'Edit Menu Item', 'carbon_fields' ); ?></a>
-					</span>
-				</div>
-			</div>
-
-			<div class="menu-item-settings" id="menu-item-settings-<?php echo esc_attr( $item_id ); ?>">
-				<?php if ( 'custom' == $item->type ) : ?>
-					<p class="field-url description description-wide">
-						<label for="edit-menu-item-url-<?php echo esc_attr( $item_id ); ?>">
-							<?php _e( 'URL', 'carbon_fields' ); ?><br />
-							<input type="text" id="edit-menu-item-url-<?php echo esc_attr( $item_id ); ?>" class="widefat code edit-menu-item-url" name="menu-item-url[<?php echo esc_attr( $item_id ); ?>]" value="<?php echo esc_attr( $item->url ); ?>" />
-						</label>
-					</p>
-				<?php endif; ?>
-				<p class="description description-wide">
-					<label for="edit-menu-item-title-<?php echo esc_attr( $item_id ); ?>">
-						<?php _e( 'Navigation Label', 'carbon_fields' ); ?><br />
-						<input type="text" id="edit-menu-item-title-<?php echo esc_attr( $item_id ); ?>" class="widefat edit-menu-item-title" name="menu-item-title[<?php echo esc_attr( $item_id ); ?>]" value="<?php echo esc_attr( $item->title ); ?>" />
-					</label>
-				</p>
-				<p class="field-title-attribute description description-wide">
-					<label for="edit-menu-item-attr-title-<?php echo esc_attr( $item_id ); ?>">
-						<?php _e( 'Title Attribute', 'carbon_fields' ); ?><br />
-						<input type="text" id="edit-menu-item-attr-title-<?php echo esc_attr( $item_id ); ?>" class="widefat edit-menu-item-attr-title" name="menu-item-attr-title[<?php echo esc_attr( $item_id ); ?>]" value="<?php echo esc_attr( $item->post_excerpt ); ?>" />
-					</label>
-				</p>
-				<p class="field-link-target description">
-					<label for="edit-menu-item-target-<?php echo esc_attr( $item_id ); ?>">
-						<input type="checkbox" id="edit-menu-item-target-<?php echo esc_attr( $item_id ); ?>" value="_blank" name="menu-item-target[<?php echo esc_attr( $item_id ); ?>]"<?php checked( $item->target, '_blank' ); ?> />
-						<?php _e( 'Open link in a new window/tab', 'carbon_fields' ); ?>
-					</label>
-				</p>
-				<p class="field-css-classes description description-thin">
-					<label for="edit-menu-item-classes-<?php echo esc_attr( $item_id ); ?>">
-						<?php _e( 'CSS Classes (optional)', 'carbon_fields' ); ?><br />
-						<input type="text" id="edit-menu-item-classes-<?php echo esc_attr( $item_id ); ?>" class="widefat code edit-menu-item-classes" name="menu-item-classes[<?php echo esc_attr( $item_id ); ?>]" value="<?php echo esc_attr( implode( ' ', $item->classes ) ); ?>" />
-					</label>
-				</p>
-				<p class="field-xfn description description-thin">
-					<label for="edit-menu-item-xfn-<?php echo esc_attr( $item_id ); ?>">
-						<?php _e( 'Link Relationship (XFN)', 'carbon_fields' ); ?><br />
-						<input type="text" id="edit-menu-item-xfn-<?php echo esc_attr( $item_id ); ?>" class="widefat code edit-menu-item-xfn" name="menu-item-xfn[<?php echo esc_attr( $item_id ); ?>]" value="<?php echo esc_attr( $item->xfn ); ?>" />
-					</label>
-				</p>
-				<p class="field-description description description-wide">
-					<label for="edit-menu-item-description-<?php echo esc_attr( $item_id ); ?>">
-						<?php _e( 'Description', 'carbon_fields' ); ?><br />
-						<textarea id="edit-menu-item-description-<?php echo esc_attr( $item_id ); ?>" class="widefat edit-menu-item-description" rows="3" cols="20" name="menu-item-description[<?php echo esc_attr( $item_id ); ?>]"><?php echo esc_html( $item->description ); // textarea_escaped ?></textarea>
-						<span class="description"><?php _e( 'The description will be displayed in the menu if the current theme supports it.', 'carbon_fields' ); ?></span>
-					</label>
-				</p>
-
-				<?php
-
-				/**
-				 * Adding Custom Hook.
-				 * This would be soon replaced by a filter, 
-				 * in which case we would not need to replace the entire walker
-				 * 
-				 * https://core.trac.wordpress.org/ticket/27280
-				 * 
-				 * (This comment marks the only change to the default walker)
-				 */
-				do_action( 'crb_print_carbon_container_nav_menu_fields_html', $item, $output, $depth, $args, $id );
-				?>
-
-				<p class="field-move hide-if-no-js description description-wide">
-					<label>
-						<span><?php _e( 'Move', 'carbon_fields' ); ?></span>
-						<a href="#" class="menus-move menus-move-up" data-dir="up"><?php _e( 'Up one', 'carbon_fields' ); ?></a>
-						<a href="#" class="menus-move menus-move-down" data-dir="down"><?php _e( 'Down one', 'carbon_fields' ); ?></a>
-						<a href="#" class="menus-move menus-move-left" data-dir="left"></a>
-						<a href="#" class="menus-move menus-move-right" data-dir="right"></a>
-						<a href="#" class="menus-move menus-move-top" data-dir="top"><?php _e( 'To the top', 'carbon_fields' ); ?></a>
-					</label>
-				</p>
-
-				<div class="menu-item-actions description-wide submitbox">
-					<?php if ( 'custom' != $item->type && $original_title !== false ) : ?>
-						<p class="link-to-original">
-							<?php printf( __( 'Original: %s', 'carbon_fields' ), '<a href="' . esc_attr( $item->url ) . '">' . esc_html( $original_title ) . '</a>' ); ?>
-						</p>
-					<?php endif; ?>
-					<a class="item-delete submitdelete deletion" id="delete-<?php echo esc_attr( $item_id ); ?>" href="<?php
-					echo wp_nonce_url(
-						add_query_arg(
-							array(
-								'action' => 'delete-menu-item',
-								'menu-item' => $item_id,
-							),
-							admin_url( 'nav-menus.php' )
-						),
-						'delete-menu_item_' . $item_id
-					); ?>"><?php _e( 'Remove', 'carbon_fields' ); ?></a> <span class="meta-sep hide-if-no-js"> | </span> <a class="item-cancel submitcancel hide-if-no-js" id="cancel-<?php echo esc_attr( $item_id ); ?>" href="<?php echo esc_url( add_query_arg( array( 'edit-menu-item' => $item_id, 'cancel' => time() ), admin_url( 'nav-menus.php' ) ) );
-						?>#menu-item-settings-<?php echo esc_attr( $item_id ); ?>"><?php _e( 'Cancel', 'carbon_fields' ); ?></a>
-				</div>
-
-				<input class="menu-item-data-db-id" type="hidden" name="menu-item-db-id[<?php echo esc_attr( $item_id ); ?>]" value="<?php echo esc_attr( $item_id ); ?>" />
-				<input class="menu-item-data-object-id" type="hidden" name="menu-item-object-id[<?php echo esc_attr( $item_id ); ?>]" value="<?php echo esc_attr( $item->object_id ); ?>" />
-				<input class="menu-item-data-object" type="hidden" name="menu-item-object[<?php echo esc_attr( $item_id ); ?>]" value="<?php echo esc_attr( $item->object ); ?>" />
-				<input class="menu-item-data-parent-id" type="hidden" name="menu-item-parent-id[<?php echo esc_attr( $item_id ); ?>]" value="<?php echo esc_attr( $item->menu_item_parent ); ?>" />
-				<input class="menu-item-data-position" type="hidden" name="menu-item-position[<?php echo esc_attr( $item_id ); ?>]" value="<?php echo esc_attr( $item->menu_order ); ?>" />
-				<input class="menu-item-data-type" type="hidden" name="menu-item-type[<?php echo esc_attr( $item_id ); ?>]" value="<?php echo esc_attr( $item->type ); ?>" />
-			</div><!-- .menu-item-settings-->
-			<ul class="menu-item-transport"></ul>
-		<?php
-		$output .= ob_get_clean();
+		$marker = '<p class="field-move hide-if-no-js description description-wide">';
+		$output = preg_replace( '~(?<!' . preg_quote( $flag, '~' ) . ')' . preg_quote( $marker, '~' ) . '~', $fields . $marker, $output );
 	}
 }
