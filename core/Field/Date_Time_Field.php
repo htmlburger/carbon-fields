e45e189143a53727d873a568ca3bddce64951f84
Marin Atanasov <tyxla@abv.bg>
2016-03-24 10:40:34 +0200

    introduce Date_Time field

---
+++ core/Field/Date_Time_Field.php
@@ -0,0 +1,10 @@
+<?php 
+
+namespace Carbon_Fields\Field;
+
+/**
+ * Date and time picker field class.
+ */
+class Date_Time_Field extends Time_Field {
+	protected $timepicker_type = 'datetimepicker';
+}
