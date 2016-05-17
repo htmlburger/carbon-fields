ac31558f9751180861beea2e0c89cca15c9f5420
XavierNuel <xavier@net-concept.fr>
2016-05-17 18:57:36 +0200

    Add Select field with multiple value support

---
+++ core/Field/Multiselect_Field.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Carbon_Fields\Field;
+
+/**
+ * Select dropdown field class wtih multiple selection enabled.
+ */
+class Multiselect_Field extends Predefined_Options_Field {
+	/**
+	 * Returns an array that holds the field data, suitable for JSON representation.
+	 * This data will be available in the Underscore template and the Backbone Model.
+	 *
+	 * @param bool $load  Should the value be loaded from the database or use the value from the current instance.
+	 * @return array
+	 */
+	public function to_json( $load ) {
+		$field_data = parent::to_json( $load );
+		$this->load_options();
+
+		$field_data = array_merge( $field_data, array(
+			'options' => $this->parse_options( $this->options ),
+		) );
+		return $field_data;
+	}
+
+	/**
+	 * The main Underscore template of this field.
+	 */
+	public function template() {
+		?>
+		<# if (_.isEmpty(options)) { #>
+			<em><?php _e( 'no options', 'carbon_fields' ); ?></em>
+		<# } else { #>
+			<select id="{{{ id }}}" name="{{{ name }}}[]" multiple {{{ special_attrs }}}>
+				<# _.each(options, function(option) { #>
+                    <option value="{{ option.value }}" {{{ value && ( value.indexOf(option.value) != -1 ) ? 'selected="selected"' : '' }}}>
+						{{{ option.name }}}
+					</option>
+				<# }) #>
+			</select>
+		<# } #>
+		<?php
+	}
+}
