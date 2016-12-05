9b2a3c2ca5a9ac2e675f8786725317a3bcdc0ad2
Viktor Vasilev <viktor.2create@gmail.com>
2016-12-05 17:58:10 +0200

    WIP: User Meta tests

---
+++ assets/tests/containers/sagas/user-meta.test.js
@@ -0,0 +1,85 @@
+import { partial } from 'lodash';
+import { put } from 'redux-saga/effects';
+
+import { createSelectboxChannel } from 'lib/events';
+
+import { TYPE_TERM_META } from 'containers/constants';
+import { setMeta, setUI } from 'containers/actions';
+import {
+	workerSyncRole,
+	workerCheckVisibility
+} from 'containers/sagas/user-meta';
+
+import {
+	stubContainerVisibilityAction,
+	stubContainerState
+} from "tests/helpers";
+
+const containerId = 'UserMetaContainer';
+
+describe('containers/sagas/user-meta', () => {
+	//////////////////////////////////
+	// Test `workerSyncRole` method //
+	//////////////////////////////////
+	describe('syncRole', () => {
+		it('should yield "SET_META" action with value from the selectbox', () => {
+			document.body.innerHTML = `
+				<select id="role">
+					<option value="subscriber">Subscriber</option>
+					<option value="contributor" selected>Contributor</option>
+					<option value="author">Author</option>
+					<option value="editor">Editor</option>
+					<option value="administrator">Administrator</option>
+				</select>
+			`;
+
+			const generator = workerSyncRole(containerId);
+
+			generator.next();
+			generator.next(createSelectboxChannel('#role'));
+
+			const actual = generator.next({
+				value: 'contributor'
+			}).value;
+
+			const expected = put(setMeta({
+				containerId: containerId,
+				meta: {
+					role: 'contributor'
+				}
+			}));
+
+			expect(actual).toEqual(expected);
+		});
+
+		it('should yield "SET_META" action with value from the data attribute', () => {
+			document.body.innerHTML = `
+				<fieldset id="${containerId}" class="container-holder carbon-user-container container-Address carbon-fields-collection" data-profile-role="administrator"></fieldset>
+			`;
+
+			const element = document.querySelector(`#${containerId}`);
+			const generator = workerSyncRole(containerId);
+
+			// At the moment jsdom doesn't support `dataset` property.
+			// See https://github.com/tmpvar/jsdom/issues/961 for more details.
+			element.dataset = {
+				profileRole: 'administrator'
+			};
+
+			generator.next();
+			generator.next(createSelectboxChannel('#role'));
+			generator.throw(''); // Manually exit from while loop.
+
+			const expected = put(setMeta({
+				containerId: containerId,
+				meta: {
+					role: 'administrator'
+				}
+			}));
+
+			const actual = generator.next(element).value;
+
+			expect(actual).toEqual(expected);
+		});
+	});
+});
