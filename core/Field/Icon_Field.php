a662dd6d9b23c877ac0cf111b7d4d51f24e3031d
atanas-angelov-dev <atanas.angelov.dev@gmail.com>
2016-12-05 18:16:09 +0200

    load fontawesome from cdn instead of distributing it with carbon fields

--- core/Field/Icon_Field.php
+++ core/Field/Icon_Field.php
@@ -31,7 +31,7 @@ class Icon_Field extends Field {
 	 * Hook administration scripts and styles.
 	 */
 	public static function admin_enqueue_scripts() {
-		wp_enqueue_style( 'fontawesome', \Carbon_Fields\URL . '/assets/fontawesome/font-awesome.min.css', array(), '4.7.0' );
+		wp_enqueue_style( 'fontawesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', array(), '4.7.0' );
 	}
 
 	/**
