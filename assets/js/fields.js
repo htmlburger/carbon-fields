d1bf1aba17cba04f21192e0aa73525c5c2f73ea9
Plamen Kostadinov <pkostadinov-2create@users.noreply.github.com>
2016-12-14 21:52:17 +0200

    Rework the javascript validation
    Replace the current implementation for forced value sanitizing, with simple validation and error triggering.

--- assets/js/fields.js
+++ assets/js/fields.js
@@ -1622,81 +1622,26 @@ window.carbon = window.carbon || {};
 		 */
 		validate: function(attrs, options) {
 			var hasErrors = false;
+			var _this = this;
+			var min = _this.get('min');
+			var max = _this.get('max');
+			var step = _this.get('step');
+			var value = attrs.value;
 
-			if (attrs.value === '') {
-				hasErrors = true;
+			var testStepValidation = ( value - min ) / step;
+
+			if ( value === '' ) {
+				hasErrors = crbl10n.message_required_field;
+			} else if ( isNaN(value) ) {
+				hasErrors = crbl10n.message_form_validation_failed;
+			} else if ( min > value || value > max ) {
+				hasErrors = crbl10n.message_form_validation_failed;
+			} else if ( testStepValidation !== parseInt( testStepValidation, 10 ) ) {
+				hasErrors = crbl10n.message_form_validation_failed;
 			}
 
 			return hasErrors;
 		}
-	});
-
-	 // Number View
-	 carbon.fields.View.Number = carbon.fields.View.extend({
-		// Add the events from the parent view and also include new ones
-		events: function() {
-			return _.extend({}, carbon.fields.View.prototype.events, {
-				'blur input[type="number"]': 'checkValue',
-			});
-		},
-
-		initialize: function() {
-			carbon.fields.View.prototype.initialize.apply(this);
-
-			this.on('field:rendered', this.initNumberField);
-		},
-
-		initNumberField: function() {
-			var _this = this;
-			var model = this.model;
-			var min = model.get('min');
-			var max = model.get('max');
-			var step = model.get('step');
-
-			if ( step == 1 ) {
-				_this.$('input').attr('step', 'any');
-			} else if ( step != 1 ) {
-				_this.$('input').attr('step', step);
-			};
-
-			_this.$('input').attr('min', min);
-			_this.$('input').attr('max', max);
-		},
-
-		stepValue: function(number, step, min) {
-			step = step || 1;
-
-			// No change needed
-			if ( (number % step).toFixed(0) ) {
-				return number;
-			};
-
-			number = number - min;
-
-			var base = number/step;
-
-			return (base * step) + min;
-		},
-
-		checkValue: function(event) {
-			var $input = this.$('input[type="number"]');
-			var value = $input.val();
-			var min = this.model.get('min');
-			var max = this.model.get('max');
-			var step = this.model.get('step');
-
-			var floatval = parseFloat(value);
-			floatval = this.stepValue(floatval, step, min);
-
-			if ( !isNaN(floatval) && min <= floatval && floatval <= max ) {
-				value = floatval;
-			} else {
-				value = '';
-			}
-
-			this.model.set('value', value);
-			$input.val(value);
-		},
 	});
 
 	/*--------------------------------------------------------------------------
