ceaf4abda29e006a68730e587d15088372cb09b0
Miroslav Mitev <miroslavmitev@gmail.com>
2016-12-14 14:31:05 +0200

    Fix set_default_value not working for the Set field
    Fixes issue #155

--- core/Field/Set_Field.php
+++ core/Field/Set_Field.php
@@ -7,7 +7,19 @@ namespace Carbon_Fields\Field;
  * Allows to create a set of checkboxes where multiple can be selected.
  */
 class Set_Field extends Predefined_Options_Field {
+	/**
+	 * The options limit.
+	 *
+	 * @var int
+	 */
 	protected $limit_options = 0;
+
+	/**
+	 * Default field value
+	 *
+	 * @var array
+	 */
+	protected $default_value = array();
 
 	/**
 	 * Set the number of the options to be displayed at the initial field display.
@@ -26,7 +38,7 @@ class Set_Field extends Predefined_Options_Field {
 	 */
 	public function get_value() {
 		if ( $this->value === false ) {
-			return array();
+			return $this->set_value( $this->default_value );
 		}
 
 		$this->load_options();
