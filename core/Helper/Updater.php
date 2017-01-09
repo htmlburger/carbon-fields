7b1d92490adcd3f9a593096de820bee6e71861b3
alpanayotov <alexander.panayotov@gmail.com>
2017-01-09 12:21:45 +0200

    Add some cosmetic updates to the Updater

--- core/Helper/Updater.php
+++ core/Helper/Updater.php
@@ -6,16 +6,46 @@ namespace Carbon_Fields\Helper;
 */
 class Updater {
 
-	public static $info = "test";
-	
-	public static function parse_update_value( $field, $value ) {
-		$type  = $field->type;
+	public static function update_post_meta( $id, $name, $value, $type = null ) {
+		$name  = Helper::prepare_meta_name( $name );
+		$value = self::parse_value( $value );
+
+		update_post_meta( $id, $name, $value );
+	}
+
+	public static function update_term_meta( $id, $name, $value, $type = null ) {
+		$name  = Helper::prepare_meta_name( $name );
+		$value = self::parse_value( $value );
+
+		update_term_meta( $id, $name, $value );	
+	}
+
+	public static function update_user_meta( $id, $name, $value, $type = null ) {
+		$name  = Helper::prepare_meta_name( $name );
+		$value = self::parse_value( $value );
+
+		update_user_meta( $id, $name, $value );	
+	}
+
+	public static function update_comment_meta( $id, $name, $value, $type = null ) {
+		$name  = Helper::prepare_meta_name( $name );
+		$value = self::parse_value( $value );
+
+		update_comment_meta( $id, $name, $value );
+	}
+
+	public static function update_theme_option( $name, $value, $type, $autoload = null ) {
+		$value = self::parse_value( $value );
+
+		update_option( $name, $value, $autoload );		
+	}
+
+	public static function parse_value( $value, $type ) {
 		$value = self::maybe_json_decode( $value );
-		$name  = $field->get_name();
 
 		switch ( $type ) {
 			case 'complex':
-				// make decision on array struckture
+				
 			break;
 
 			case 'map':
@@ -47,20 +77,4 @@ class Updater {
 	public static function is_json( $string ) {
 		return json_decode( $string );
 	}	
-
-	public static function update_post_meta( $id, $name, $value, $type = null ) {
-		update_post_meta( $id, $name, $value );
-	}
-
-	public static function update_term_meta( $id, $name, $value, $type = null ) {
-		update_term_meta( $id, $name, $value );	
-	}
-
-	public static function update_user_meta( $id, $name, $value, $type = null ) {
-		update_user_meta( $id, $name, $value );	
-	}
-
-	public static function update_theme_option( $name, $value, $type ) {
-		update_theme_option( $name, $value );		
-	}
 }
