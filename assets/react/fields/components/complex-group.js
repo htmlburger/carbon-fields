59166e69ee020469d2752ccd7d2f5fc1cef6547b
Viktor Vasilev <viktor.2create@gmail.com>
2017-01-13 16:29:33 +0200

    Add ability to collapse/expand a complex group

--- assets/react/fields/components/complex-group.js
+++ assets/react/fields/components/complex-group.js
@@ -1,18 +1,29 @@
+/**
+ * The external dependencies.
+ */
 import React from 'react';
+import cx from 'classnames';
+import { compose, withHandlers, withState } from 'recompose';
+
+/**
+ * The internal dependencies.
+ */
 import { makeField } from 'lib/factory';
 
 /**
- * @param  {Object} props
- * @param  {Object} props.complex
- * @param  {Object} props.group
- * @param  {Number} props.index
+ * @param  {Object}   props
+ * @param  {Object}   props.complex
+ * @param  {Object}   props.group
+ * @param  {Number}   props.index
+ * @param  {Boolean}  props.collapsed
+ * @param  {Function} props.toggle
  * @return {React.Element}
  *
  * @todo Fix the translation.
  * @todo Fix the custom label.
  */
-export const ComplexGroup = ({ complex, group, index }) => {
-	return <div id={`carbon-${complex.name}-complex-container`} className="carbon-row carbon-group-row">
+export const ComplexGroup = ({ complex, group, index, collapsed, toggle }) => {
+	return <div id={`carbon-${complex.name}-complex-container`} className={cx('carbon-row', 'carbon-group-row', { 'collapsed': collapsed })}>
 		<input
 			type="hidden"
 			name={`${complex.name}[${index}][group]`}
@@ -33,7 +44,7 @@ export const ComplexGroup = ({ complex, group, index }) => {
 				Remove
 			</a>
 
-			<a className="carbon-btn-collapse dashicons-before dashicons-arrow-up" href="#" title="Collapse/Expand">
+			<a className="carbon-btn-collapse dashicons-before dashicons-arrow-up" href="#" title="Collapse/Expand" onClick={toggle}>
 				Collapse/Expand
 			</a>
 		</div>
@@ -44,5 +55,31 @@ export const ComplexGroup = ({ complex, group, index }) => {
 	</div>;
 };
 
-export default ComplexGroup;
+/**
+ * Expand or collapse the group.
+ *
+ * @param  {Object}   props
+ * @param  {Boolean}  props.collapsed
+ * @param  {Function} props.setCollapsed
+ * @return {Function}
+ */
+const toggle = ({ collapsed, setCollapsed }) => {
+	/**
+	 * @inner
+	 * @param  {Event} e
+	 * @return {void}
+	 */
+	return (e) => {
+		e.preventDefault();
+
+		setCollapsed(!collapsed);
+	};
+}
+
+export default compose(
+	withState('collapsed', 'setCollapsed', false),
+	withHandlers({
+		toggle
+	})
+)(ComplexGroup);
 
