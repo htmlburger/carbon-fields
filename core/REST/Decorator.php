63f9b704b2ff16453c2de922e6e10ce6a11b0243
alpanayotov <alexander.panayotov@gmail.com>
2017-01-17 15:39:09 +0200

    Fix loading and updating fields with the same name but in different context

--- core/REST/Decorator.php
+++ core/REST/Decorator.php
@@ -41,15 +41,15 @@ class Decorator {
 		$this->fields = call_user_func_array( 'array_merge', $this->fields );
 
 		foreach ( $containers as $container ) {
-			$fields        = $this->filter_fields( $container );
-			$type_to_lower = strtolower( $container->type );
-			$types         = call_user_func( [ __CLASS__, "get_{$type_to_lower}_container_settings" ], $container );
+			$fields  = $this->filter_fields( $container );
+			$context = strtolower( $container->type );
+			$types   = call_user_func( [ __CLASS__, "get_{$context}_container_settings" ], $container );
 
 			foreach ( $fields as $field ) {
 				register_rest_field( $types,
 					$field->get_name(), [
-						'get_callback'    => [ $this, 'load_field_value' ],
-						'update_callback' => [ $this, 'update_field_value' ],
+						'get_callback'    => [ $this, "load_{$context}_field_value" ],
+						'update_callback' => [ $this, "update_{$context}_field_value" ],
 						'schema'          => null,
 					]
 				);
@@ -81,16 +81,72 @@ class Decorator {
 	}
 
 	/**
+	 * Wrapper method for loading Post Meta values, 
+	 * needed to pass the correct $context
+	 * 
+	 * @param array $object Details of current object.
+	 * @param string $field_name Name of field.
+	 * @param WP_REST_Request $request Current request
+	 * @return mixed
+	 */
+	public function load_post_meta_field_value( $object, $field_name, $request ) {
+		$context = 'Post_Meta';
+		return $this->load_field_value( $object, $field_name, $request, $context );
+	}
+	
+	/**
+	 * Wrapper method for loading Term Meta values, 
+	 * needed to pass the correct $context
+	 * 
+	 * @param array $object Details of current object.
+	 * @param string $field_name Name of field.
+	 * @param WP_REST_Request $request Current request
+	 * @return mixed
+	 */
+	public function load_term_meta_field_value( $object, $field_name, $request ) {
+		$context = 'Term_Meta';
+		return $this->load_field_value( $object, $field_name, $request, $context );
+	}
+
+	/**
+	 * Wrapper method for loading Comment Meta values, 
+	 * needed to pass the correct $context
+	 * 
+	 * @param array $object Details of current object.
+	 * @param string $field_name Name of field.
+	 * @param WP_REST_Request $request Current request
+	 * @return mixed
+	 */
+	public function load_comment_meta_field_value( $object, $field_name, $request ) {
+		$context = 'Comment_Meta';
+		return $this->load_field_value( $object, $field_name, $request, $context );
+	}
+
+	/**
+	 * Wrapper method for loading User Meta values, 
+	 * needed to pass the correct $context
+	 * 
+	 * @param array $object Details of current object.
+	 * @param string $field_name Name of field.
+	 * @param WP_REST_Request $request Current request
+	 * @return mixed
+	 */
+	public function load_user_meta_field_value( $object, $field_name, $request ) {
+		$context = 'User_Meta';
+		return $this->load_field_value( $object, $field_name, $request, $context );
+	}
+
+	/**
 	 * Get the value of the "$field_name" field
 	 *
 	 * @param array $object Details of current object.
  	 * @param string $field_name Name of field.
  	 * @param WP_REST_Request $request Current request
+ 	 * @param string $context Post_Meta|Term_Meta|User_Meta|Comment_Meta
  	 * @return mixed
 	 */	
-	public function load_field_value( $object, $field_name, $request ) {
-
-		$field = $this->get_current_field( $field_name );
+	public function load_field_value( $object, $field_name, $request, $context ) {
+		$field = $this->get_current_field( $field_name, $context );
 
 		if ( empty( $field ) ) {
 			return '';
@@ -107,33 +163,85 @@ class Decorator {
 	}
 
 	/**
+	 * Wrapper method for updating Post Meta fields,
+	 * need to pass the correct $context
+	 * 
+	 * @param mixed $value The value of the field
+	 * @param object $object The object from the request
+	 * @param string $field_name Name of field
+	 */
+	public function update_post_meta_field_value( $value, $object, $field_name ) {
+		$context = 'Post_Meta';
+		$this->update_field_value( $value, $object, $field_name, $context );
+	}
+
+	/**
+	 * Wrapper method for updating Term Meta fields,
+	 * need to pass the correct $context
+	 * 
+	 * @param mixed $value The value of the field
+	 * @param object $object The object from the request
+	 * @param string $field_name Name of field
+	 */
+	public function update_term_meta_field_value( $value, $object, $field_name ) {
+		$context = 'Term_Meta';
+		$this->update_field_value( $value, $object, $field_name, $context );
+	}
+
+	/**
+	 * Wrapper method for updating Comment Meta fields,
+	 * need to pass the correct $context
+	 * 
+	 * @param mixed $value The value of the field
+	 * @param object $object The object from the request
+	 * @param string $field_name Name of field
+	 */
+	public function update_comment_meta_field_value( $value, $object, $field_name ) {
+		$context = 'Comment_Meta';
+		$this->update_field_value( $value, $object, $field_name, $context );
+	}
+
+	/**
+	 * Wrapper method for updating User Meta fields,
+	 * need to pass the correct $context
+	 * 
+	 * @param mixed $value The value of the field
+	 * @param object $object The object from the request
+	 * @param string $field_name Name of field
+	 */
+	public function update_user_meta_field_value( $value, $object, $field_name ) {
+		$context = 'User_Meta';
+		$this->update_field_value( $value, $object, $field_name, $context );
+	}
+
+	/**
 	 * Handler for updating custom field data.
 	 *
 	 * @param mixed $value The value of the field
-	 * @param object $object The object from the response
+	 * @param object $object The object from the request
 	 * @param string $field_name Name of field
-	 *
-	 * @return bool|int
+	 * @param string $context Post_Meta|Term_Meta|User_Meta|Comment_Meta
 	 */
-	public function update_field_value( $value, $object, $field_name ) {
+	public function update_field_value( $value, $object, $field_name, $context ) {
 		if ( ! $value || ! is_string( $value ) ) {
 			return;
 		}
-
-		$field = $this->get_current_field( $field_name );
+		
+		$field = $this->get_current_field( $field_name, $context );
 
 		if ( empty( $field ) ) {
 			return;
 		}
 		
-		$field    = array_pop( $field );
-		$type     = strtolower( $field->type );
-		$context  = strtolower( $field->get_context() ); 
+		$field     = array_pop( $field );
+		$type      = strtolower( $field->type );
+		$context   = strtolower( $context );
+		$object_id = self::get_object_id( $object, $context );
 
 		Updater::$is_rest_request = true;
-
+		
 		try {
-			call_user_func( "Carbon_Fields\Updater\Updater::update_field", $context, $object->ID, $field_name, $value, $type );	
+			call_user_func( "Carbon_Fields\Updater\Updater::update_field", $context, $object_id, $field_name, $value, $type );	
 		} catch ( \Exception $e ) {
 			echo wp_strip_all_tags( $e->getMessage() );
 			exit;
@@ -156,9 +264,9 @@ class Decorator {
 	 * @param  string $field_name 
 	 * @return array
 	 */
-	public function get_current_field( $field_name ) {
-		return array_filter( $this->fields, function( $field ) use ( $field_name ) { 
-			return $field->get_name() === $field_name;
+	public function get_current_field( $field_name, $context ) {
+		return array_filter( $this->fields, function( $field ) use ( $field_name, $context ) { 
+			return ( $field->get_name() === $field_name ) && ( $field->get_context() === $context );
 		} );
 	}
 
@@ -198,4 +306,28 @@ class Decorator {
 		return 'comment';
 	}
 
+	/**
+	 * Retrieve ID from object
+	 * based on $context
+	 * 
+	 * @param  object $object
+	 * @param  string $context
+	 * @return string
+	 */
+	public static function get_object_id( $object, $context ) {
+		switch ( $context ) {
+			case 'post_meta': // fallthrough intended
+			case 'user_meta': 
+				return $object->ID;
+				break;
+
+			case 'term_meta':
+				return $object->term_id;
+				break;
+
+			case 'comment_meta' : 
+				return $object->comment_ID;
+				break;
+		}
+	}
 }
