b5911fda1b31b6e2d5651ce3077da314941b818a
apanaiotov <apanaiotov@htmlbox.net>
2017-01-18 16:46:06 +0200

    Update arguments naming

--- assets/react/fields/actions.js
+++ assets/react/fields/actions.js
@@ -6,37 +6,37 @@ import { createAction } from 'redux-actions';
 /**
  * Perform the initial setup of the container.
  *
- * @param  {String} id
+ * @param  {String} fieldId
  * @param  {String} type
  * @return {Object}
  */
 export const SETUP_FIELD = 'fields/SETUP_FIELD';
-export const setupField = createAction(SETUP_FIELD, (id, type) => ({ id, type }));
+export const setupField = createAction(SETUP_FIELD, (fieldId, type) => ({ fieldId, type }));
 
 /**
  * Update the object that contains information about field's UI.
  *
- * @param  {String} id
+ * @param  {String} fieldId
  * @param  {Object} ui
  * @return {Object}
  */
 export const SET_UI = 'fields/SET_UI';
-export const setUI = createAction(SET_UI, (id, ui) => ({ id, ui }));
+export const setUI = createAction(SET_UI, (fieldId, ui) => ({ fieldId, ui }));
 
 /**
  * Update the field.
  *
- * @param  {String} id
+ * @param  {String} fieldId
  * @param  {Object} values
  * @return {Object}
  */
 export const UPDATE_FIELD = 'fields/UPDATE_FIELD';
-export const updateField = createAction(UPDATE_FIELD, (id, values) => ({ id, values }));
+export const updateField = createAction(UPDATE_FIELD, (fieldId, values) => ({ fieldId, values }));
 
 /**
  * Setup the interaction between field and WordPress's media browser.
  *
- * @param  {String} id
+ * @param  {String} fieldId
  * @return {Object}
  */
 export const SETUP_MEDIA_BROWSER = 'fields/SETUP_MEDIA_BROWSER';
@@ -45,7 +45,7 @@ export const setupMediaBrowser = createAction(SETUP_MEDIA_BROWSER);
 /**
  * Open the built-in media browser.
  *
- * @param  {String} id
+ * @param  {String} fieldId
  * @return {Object}
  */
 export const OPEN_MEDIA_BROWSER = 'fields/OPEN_MEDIA_BROWSER';
@@ -72,19 +72,19 @@ export const removeFields = createAction(REMOVE_FIELDS);
 /**
  * Add a new instance of the specified complex group.
  *
- * @param  {String} id
- * @param  {String} group
+ * @param  {String} fieldId
+ * @param  {String} groupName
  * @return {Object}
  */
 export const ADD_COMPLEX_GROUP = 'fields/ADD_COMPLEX_GROUP';
-export const addComplexGroup = createAction(ADD_COMPLEX_GROUP, (id, group) => ({ id, group }));
+export const addComplexGroup = createAction(ADD_COMPLEX_GROUP, (fieldId, groupName) => ({ fieldId, groupName }));
 
 /**
  * Remove the specified complex group.
  *
- * @param  {String} id
- * @param  {String} group
+ * @param  {String} fieldId
+ * @param  {String} groupId
  * @return {Object}
  */
 export const REMOVE_COMPLEX_GROUP = 'fields/REMOVE_COMPLEX_GROUP';
-export const removeComplexGroup = createAction(REMOVE_COMPLEX_GROUP, (id, group) => ({ id, group }));
+export const removeComplexGroup = createAction(REMOVE_COMPLEX_GROUP, (fieldId, groupId) => ({ fieldId, groupId }));
