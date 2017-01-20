be3653b1a47841242794fd8c19441e00c6b32209
Viktor Vasilev <viktor.2create@gmail.com>
2017-01-20 16:27:14 +0200

    Remove the 'field' suffix from the filenames

--- assets/react/registrar.js
+++ assets/react/registrar.js
@@ -6,18 +6,18 @@ import { registerFieldComponent } from 'lib/registry';
 /**
  * Register the fields.
  */
-import CheckboxField from 'fields/components/checkbox-field';
-import ColorField from 'fields/components/color-field';
+import CheckboxField from 'fields/components/checkbox';
+import ColorField from 'fields/components/color';
 import ComplexField from 'fields/components/complex';
-import FileField from 'fields/components/file-field';
-import HtmlField from 'fields/components/html-field';
-import RadioField from 'fields/components/radio-field';
-import RadioImageField from 'fields/components/radio-image-field';
-import SelectField from 'fields/components/select-field';
+import FileField from 'fields/components/file';
+import HtmlField from 'fields/components/html';
+import RadioField from 'fields/components/radio';
+import RadioImageField from 'fields/components/radio-image';
+import SelectField from 'fields/components/select';
 import SeparatorField from 'fields/components/separator';
-import SetField from 'fields/components/set-field';
-import TextField from 'fields/components/text-field';
-import TextareaField from 'fields/components/textarea-field';
+import SetField from 'fields/components/set';
+import TextField from 'fields/components/text';
+import TextareaField from 'fields/components/textarea';
 
 registerFieldComponent('Checkbox', CheckboxField);
 registerFieldComponent('Color', ColorField);
