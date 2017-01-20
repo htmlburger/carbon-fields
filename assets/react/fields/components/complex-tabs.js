9e9cc32b36d0ba6a617b1770138ca7cfeea0ddd7
Viktor Vasilev <viktor.2create@gmail.com>
2017-01-20 10:19:42 +0200

    Clean up the interface of the complex tabs component

--- assets/react/fields/components/complex-tabs.js
+++ assets/react/fields/components/complex-tabs.js
@@ -1,29 +1,33 @@
 /**
  * The external dependencies.
  */
-import React from 'react';
+import React, { PropTypes } from 'react';
 import cx from 'classnames';
-import { PropTypes } from 'react';
 import { compose, branch, renderNothing, renderComponent, withHandlers } from 'recompose';
+
+/**
+ * The internal dependencies.
+ */
+import { preventDefault } from 'lib/helpers';
 
 /**
  * Render the navigation of tabs when the layout of complex field is tabbed.
  *
  * @param  {Object}        props
  * @param  {Object[]}      props.groups
- * @param  {Function}      props.isActive
+ * @param  {Function}      props.isTabActive
  * @param  {Function}      props.handleClick
  * @param  {React.Element} props.children
  * @return {React.Element}
  *
  * @todo Add support for custom labels with `dangerouslySetInnerHTML`.
  */
-export const ComplexTabs = ({ groups, isActive, handleClick, children }) => {
+export const ComplexTabs = ({ groups, isTabActive, handleClick, children }) => {
 	return <div className="group-tabs-nav-holder">
 		<ul className="group-tabs-nav">
 			{
 				groups.map((group, index) => {
-					return <li key={index} className={cx('group-tab-item', { 'active': isActive(group) })}>
+					return <li key={index} className={cx('group-tab-item', { 'active': isTabActive(group.id) })}>
 						<a href="#" onClick={handleClick(group.id)}>
 							<span className="group-handle"></span>
 
@@ -58,6 +62,7 @@ ComplexTabs.propTypes = {
 	show: PropTypes.bool.isRequired,
 	current: PropTypes.string,
 	onClick: PropTypes.func.isRequired,
+	children: PropTypes.element,
 };
 
 /**
@@ -67,16 +72,7 @@ ComplexTabs.propTypes = {
  * @param  {String}   props.current
  * @return {Function}
  */
-const isActive = ({ current }) => {
-	/**
-	 * @inner
-	 * @param  {Object} group
-	 * @return {Boolean}
-	 */
-	return (group) => {
-		return group.id === current;
-	};
-};
+const isTabActive = ({ current }) => groupId => groupId === current;
 
 /**
  * Handle the click on the links in the list.
@@ -85,25 +81,13 @@ const isActive = ({ current }) => {
  * @param  {Function} props.onClick
  * @return {Function}
  */
-const handleClick = ({ onClick }) => {
-	/**
-	 * @inner
-	 * @param  {String} group
-	 * @param  {Event}  e
-	 * @return {void}
-	 */
-	return (group) => (e) => {
-		e.preventDefault();
-
-		onClick(group);
-	};
-};
+const handleClick = ({ onClick }) => groupId => preventDefault(() => onClick(groupId));
 
 export default branch(
 	({ show }) => show,
 
 	renderComponent(compose(
-		withHandlers({ isActive, handleClick })
+		withHandlers({ isTabActive, handleClick })
 	)(ComplexTabs)),
 
 	renderNothing,
