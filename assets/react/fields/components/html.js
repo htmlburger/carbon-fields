6cbe39285e7a8a442a3244d0e13765c37bf210a3
Viktor Vasilev <viktor.2create@gmail.com>
2017-01-24 16:26:47 +0200

    Add prop types to the html field

--- assets/react/fields/components/html.js
+++ assets/react/fields/components/html.js
@@ -1,6 +1,12 @@
-import React from 'react';
+/**
+ * The external dependencies.
+ */
+import React, { PropTypes } from 'react';
 import { compose } from 'recompose';
 
+/**
+ * The internal dependencies.
+ */
 import Field from 'fields/components/field';
 import withStore from 'fields/decorators/with-store';
 import withSetup from 'fields/decorators/with-setup';
@@ -16,6 +22,17 @@ export const HtmlField = ({ field }) => {
 	return <Field field={field}>
 		<div dangerouslySetInnerHTML={{ __html: field.html }}></div>
 	</Field>;
+};
+
+/**
+ * Validate the props.
+ *
+ * @type {Object}
+ */
+HtmlField.propTypes = {
+	field: PropTypes.shape({
+		html: PropTypes.string.isRequired,
+	}).isRequired,
 };
 
 export default compose(
