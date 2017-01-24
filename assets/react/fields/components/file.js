4e378c72f48060c97bb02a7f15d89b40be619c01
Viktor Vasilev <viktor.2create@gmail.com>
2017-01-24 16:48:46 +0200

    Add prop types for the file field

--- assets/react/fields/components/file.js
+++ assets/react/fields/components/file.js
@@ -1,11 +1,17 @@
-import React from 'react';
+/**
+ * The external dependencies.
+ */
+import React, { PropTypes } from 'react';
 import cx from 'classnames';
 import { compose, withHandlers } from 'recompose';
 
-import { setupMediaBrowser, openMediaBrowser } from 'fields/actions';
+/**
+ * The internal dependencies.
+ */
 import Field from 'fields/components/field';
 import withStore from 'fields/decorators/with-store';
 import withSetup from 'fields/decorators/with-setup';
+import { setupMediaBrowser, openMediaBrowser } from 'fields/actions';
 
 /**
  * Render a file upload field with a preview thumbnail of the uploaded file.
@@ -17,7 +23,8 @@ import withSetup from 'fields/decorators/with-setup';
  * @param  {Function} props.clearSelection
  * @return {React.Element}
  *
- * @todo   Replace the inline `style` with a class.
+ * TODO: Replace the inline `style` with a class.
+ * TODO: Add support for URL.
  */
 export const FileField = ({ name, field, openBrowser, clearSelection }) => {
 	return <Field field={field}>
@@ -27,10 +34,9 @@ export const FileField = ({ name, field, openBrowser, clearSelection }) => {
 				className="regular-text carbon-file-field"
 				id={field.id}
 				name={name}
-				defaultValue={field.value}
 				value={field.value}
 				style={field.value_type === 'id' ? { display: 'none' } : {}}
-			/>
+				onChange={() => {}} />
 
 			<div className={cx('carbon-description', { 'hidden': !field.value })}>
 				<div className={cx('carbon-attachment-preview', { 'hidden': !field.thumb_url })}>
@@ -47,6 +53,28 @@ export const FileField = ({ name, field, openBrowser, clearSelection }) => {
 			</span>
 		</div>
 	</Field>;
+};
+
+/**
+ * Validate the props.
+ *
+ * @type {Object}
+ */
+FileField.propTypes = {
+	name: PropTypes.string.isRequired,
+	field: PropTypes.shape({
+		id: PropTypes.string.isRequired,
+		value: PropTypes.oneOfType([
+			PropTypes.string,
+			PropTypes.number,
+		]),
+		value_type: PropTypes.string.isRequired,
+		thumb_url: PropTypes.string,
+		file_name: PropTypes.string,
+		button_label: PropTypes.string.isRequired,
+	}).isRequired,
+	openBrowser: PropTypes.func.isRequired,
+	clearSelection: PropTypes.func.isRequired,
 };
 
 /**
