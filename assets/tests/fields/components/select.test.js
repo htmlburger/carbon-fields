30bd8c61878c1a280224ffa2afc7f8ec10371532
Viktor Vasilev <viktor.2create@gmail.com>
2017-01-24 14:54:43 +0200

    Add tests for the select field

---
+++ assets/tests/fields/components/select.test.js
@@ -0,0 +1,141 @@
+/**
+ * The external dependencies.
+ */
+import React from 'react';
+import { Provider } from 'react-redux';
+import { assignIn } from 'lodash';
+
+import configureMockStore from 'redux-mock-store';
+import { shallow, mount } from 'enzyme';
+
+/**
+ * The internal dependencies.
+ */
+import ComposedSelectField, { SelectField } from 'fields/components/select';
+import { updateField } from 'fields/actions';
+
+/**
+ * The unit tests.
+ */
+describe('SelectField - Unit', () => {
+	function setup(props = {}) {
+		props = assignIn({}, {
+			name: 'field',
+			field: {
+				id: 'field-1',
+				value: 'option-1',
+				options: [
+					{
+						name: 'Option 1',
+						value: 'option-1'
+					},
+					{
+						name: 'Option 2',
+						value: 'option-2'
+					}
+				]
+			},
+			handleChange: jest.fn(),
+		}, props);
+
+		const wrapper = shallow(<SelectField {...props} />);
+
+		return { wrapper, props };
+	}
+
+	it('should render a `select` element', () => {
+		const { wrapper } = setup();
+
+		const node = wrapper.find('select');
+		const children = node.children();
+		const child = children.first();
+
+		expect(node.length).toEqual(1);
+		expect(node.prop('id')).toEqual('field-1');
+		expect(node.prop('value')).toEqual('option-1');
+
+		expect(children.length).toEqual(2);
+		expect(child.prop('value')).toEqual('option-1');
+		expect(child.text()).toEqual('Option 1');
+	});
+
+	it('should accept a `name` property', () => {
+		const { wrapper } = setup();
+		const node = wrapper.find('select');
+
+		expect(node.prop('name')).toEqual('field');
+	});
+
+	it('should accept an `onChange` handler', () => {
+		const { wrapper, props: { handleChange } } = setup();
+		const node = wrapper.find('select');
+
+		node.simulate('change');
+
+		expect(handleChange).toHaveBeenCalled();
+		expect(handleChange).toHaveBeenCalledTimes(1);
+	});
+});
+
+/**
+ * The integration tests.
+ */
+describe('SelectField - Integration', () => {
+	function setup(state = {}) {
+		const store = configureMockStore()({
+			fields: {
+				'field-1': assignIn({}, {
+					id: 'field-1',
+					options: [
+						{
+							name: 'Option 1',
+							value: 'option-1'
+						},
+						{
+							name: 'Option 2',
+							value: 'option-2'
+						}
+					]
+				}, state)
+			}
+		});
+
+		const wrapper = mount(
+			<Provider store={store}>
+				<ComposedSelectField id="field-1" />
+			</Provider>
+		);
+
+		return { wrapper, store };
+	}
+
+	it('should update the store', () => {
+		const { wrapper, store } = setup();
+
+		wrapper.find('select').simulate('change', {
+			target: {
+				value: 'option-2'
+			}
+		});
+
+		expect(store.getActions()).toEqual(jasmine.arrayContaining([
+			updateField('field-1', { value: 'option-2' })
+		]));
+	});
+
+	it('should not render a `select` element when doesn`t have options', () => {
+		const { wrapper } = setup({
+			options: []
+		});
+
+		expect(wrapper.find('select').length).toEqual(0);
+	});
+
+	it('should use the value of the first option as fallback', () => {
+		const { wrapper, store } = setup();
+
+		expect(store.getActions()).toEqual(jasmine.arrayContaining([
+			updateField('field-1', { value: 'option-1' })
+		]));
+	});
+});
