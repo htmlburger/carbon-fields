4477b9cb734593f9da6236e617fdc758182fcae1
Viktor Vasilev <viktor.2create@gmail.com>
2017-02-02 12:37:15 +0200

    Add missing DLL imports

--- webpack.app.js
+++ webpack.app.js
@@ -8,9 +8,10 @@ module.exports = {
 		'carbon.app': [
 			// Load the core library.
 			'./assets/react/lib/constants.js',
-			'./assets/react/lib/normalize.js',
-			'./assets/react/lib/factory.js',
 			'./assets/react/lib/events.js',
+			'./assets/react/lib/factory.js',
+			'./assets/react/lib/helpers.js',
+			'./assets/react/lib/normalize.js',
 			'./assets/react/lib/registry.js',
 
 			// Load the containers.
@@ -40,33 +41,50 @@ module.exports = {
 			'./assets/react/sidebars/reducer.js',
 
 			// Load the fields.
+			'./assets/react/fields/reducer',
+			'./assets/react/fields/actions',
 			'./assets/react/fields/selectors',
+			'./assets/react/fields/helpers',
 
 			'./assets/react/fields/decorators/with-store.js',
 			'./assets/react/fields/decorators/with-setup.js',
 
 			'./assets/react/fields/sagas/media-browser.js',
+			'./assets/react/fields/sagas/geocoder.js',
 			'./assets/react/fields/sagas/complex.js',
 
 			'./assets/react/fields/components/field.js',
+			'./assets/react/fields/components/association.js',
+			'./assets/react/fields/components/checkbox.js',
+			'./assets/react/fields/components/color.js',
+			'./assets/react/fields/components/complex.js',
+			'./assets/react/fields/components/datetime.js',
 			'./assets/react/fields/components/file.js',
+			'./assets/react/fields/components/html.js',
+			'./assets/react/fields/components/map.js',
+			'./assets/react/fields/components/radio.js',
+			'./assets/react/fields/components/rich-text.js',
+			'./assets/react/fields/components/select.js',
+			'./assets/react/fields/components/separator.js',
+			'./assets/react/fields/components/set.js',
 			'./assets/react/fields/components/text.js',
 			'./assets/react/fields/components/textarea.js',
-			'./assets/react/fields/components/separator.js',
-			'./assets/react/fields/components/color.js',
-			'./assets/react/fields/components/checkbox.js',
-			'./assets/react/fields/components/complex.js',
-			'./assets/react/fields/components/radio.js',
-			'./assets/react/fields/components/select.js',
-			'./assets/react/fields/components/set.js',
-			'./assets/react/fields/components/html.js',
 
-			'./assets/react/fields/components/no-options.js',
-			'./assets/react/fields/components/complex-group.js',
+			'./assets/react/fields/components/association-list-item.js',
+			'./assets/react/fields/components/association-list.js',
+			'./assets/react/fields/components/colorpicker.js',
 			'./assets/react/fields/components/complex-actions.js',
+			'./assets/react/fields/components/complex-empty-notice.js',
+			'./assets/react/fields/components/complex-group.js',
 			'./assets/react/fields/components/complex-popover.js',
 			'./assets/react/fields/components/complex-tabs.js',
-			'./assets/react/fields/components/complex-empty-notice.js',
+			'./assets/react/fields/components/datetimepicker.js',
+			'./assets/react/fields/components/google-map.js',
+			'./assets/react/fields/components/no-options.js',
+			'./assets/react/fields/components/radio-image-list.js',
+			'./assets/react/fields/components/radio-list.js',
+			'./assets/react/fields/components/rich-text-editor.js',
+			'./assets/react/fields/components/search-input.js',
 
 			// Load the store.
 			'./assets/react/store.js'
