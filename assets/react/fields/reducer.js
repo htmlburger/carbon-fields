1359660b66e244ea95f91bcceff58bb61f32b898
Viktor Vasilev <viktor.2create@gmail.com>
2017-02-07 16:44:18 +0200

    Refactor the fields reducer

--- assets/react/fields/reducer.js
+++ assets/react/fields/reducer.js
@@ -1,30 +1,23 @@
+/**
+ * The external dependencies.
+ */
 import immutable from 'object-path-immutable';
+import { handleActions } from 'redux-actions';
 import { omit } from 'lodash';
-import { decorateFieldReducer } from 'lib/registry';
-import { ADD_FIELDS, REMOVE_FIELDS, UPDATE_FIELD, SET_UI, updateValue } from 'fields/actions';
 
 /**
- * The reducer that handles manipulation to field's state.
- *
- * @param  {Object} [state]
- * @param  {Object} [action]
- * @return {Object}
+ * The internal dependencies.
  */
-function reducer(state = {}, { type, payload } = {}) {
-	switch (type) {
-		case ADD_FIELDS:
-			return { ...state, ...payload };
-		case REMOVE_FIELDS:
-			return omit(state, payload);
-		case UPDATE_FIELD:
-			return immutable.assign(state, payload.fieldId, payload.values);
-		case SET_UI:
-			return immutable.assign(state, `${payload.fieldId}.ui`, payload.ui);
-		case 'fields/UPDATE_VALUE':
-			return immutable.set(state, `${payload.fieldId}.value`, payload.value);
-		default:
-			return state;
-	}
-}
+import { decorateFieldReducer } from 'lib/registry';
+import { addFields, removeFields, updateField, setUI, updateValue } from 'fields/actions';
 
-export default decorateFieldReducer(reducer);
+/**
+ * The reducer that handles the `fields` branch.
+ */
+export default decorateFieldReducer(handleActions({
+	[addFields]: (state, { payload }) => ({ ...state, ...payload }),
+	[removeFields]: (state, { payload }) => omit(state, payload),
+	[updateField]: (state, { payload: { fieldId, values }}) => immutable.assign(state, fieldId, values),
+	[setUI]: (state, { payload: { fieldId, ui }}) => immutable.assign(state, `${fieldId}.ui`, ui),
+	[updateValue]: (state, { payload: { fieldId, value }}) => immutable.set(state, `${fieldId}.value`, value),
+}, {}));
