c1ba36dc2e3a08f5765ba6df5ddabcc8d9acbadf
atanas-angelov-dev <atanas.angelov.dev@gmail.com>
2017-02-15 12:25:39 +0200

    split the get_options method into smaller managable methods

--- core/Field/Association_Field.php
+++ core/Field/Association_Field.php
@@ -99,7 +99,7 @@ class Association_Field extends Relationship_Field {
 	/**
 	 * Converts the field values into a usable associative array.
 	 *
-	 * The relationship data is saved in the database in the following format:
+	 * The association data is saved in the database in the following format:
 	 * 	array (
 	 *		0 => 'post:page:4',
 	 *		1 => 'term:category:2',
@@ -130,7 +130,7 @@ class Association_Field extends Relationship_Field {
 	/**
 	 * Used to get the title of an item.
 	 *
-	 * Can be overriden or extended by the `carbon_relationship_title` filter.
+	 * Can be overriden or extended by the `carbon_association_title` filter.
 	 *
 	 * @param int     $id      The database ID of the item.
 	 * @param string  $type    Item type (post, term, user, comment, or a custom one).
@@ -149,7 +149,7 @@ class Association_Field extends Relationship_Field {
 			$title = get_the_author_meta( 'user_login', $id );
 		} elseif ( $type === 'comment' ) {
 			$title = get_comment_text( $id );
-			$max = apply_filters( 'carbon_relationship_comment_length', 30, $this->get_name() );
+			$max = apply_filters( 'carbon_association_comment_length', 30, $this->get_name() );
 			if ( strlen( $title ) > $max ) {
 				$title = substr( $title, 0, $max ) . '...';
 			}
@@ -160,21 +160,21 @@ class Association_Field extends Relationship_Field {
 		}
 
 		/**
-		 * Filter the title of the relationship item.
+		 * Filter the title of the association item.
 		 *
 		 * @param string $title   The unfiltered item title.
-		 * @param string $name    Name of the relationship field.
+		 * @param string $name    Name of the association field.
 		 * @param int    $id      The database ID of the item.
 		 * @param string $type    Item type (post, term, user, comment, or a custom one).
 		 * @param string $subtype Subtype - "page", "post", "category", etc.
 		 */
-		return apply_filters( 'carbon_relationship_title', $title, $this->get_name(), $id, $type, $subtype );
+		return apply_filters( 'carbon_association_title', $title, $this->get_name(), $id, $type, $subtype );
 	}
 
 	/**
 	 * Used to get the label of an item.
 	 *
-	 * Can be overriden or extended by the `carbon_relationship_item_label` filter.
+	 * Can be overriden or extended by the `carbon_association_item_label` filter.
 	 *
 	 * @param int     $id      The database ID of the item.
 	 * @param string  $type    Item type (post, term, user, comment, or a custom one).
@@ -193,149 +193,172 @@ class Association_Field extends Relationship_Field {
 		}
 
 		/**
-		 * Filter the label of the relationship item.
+		 * Filter the label of the association item.
 		 *
 		 * @param string $label   The unfiltered item label.
-		 * @param string $name    Name of the relationship field.
+		 * @param string $name    Name of the association field.
 		 * @param int    $id      The database ID of the item.
 		 * @param string $type    Item type (post, term, user, comment, or a custom one).
 		 * @param string $subtype Subtype - "page", "post", "category", etc.
 		 */
-		return apply_filters( 'carbon_relationship_item_label', $label, $this->get_name(), $id, $type, $subtype );
+		return apply_filters( 'carbon_association_item_label', $label, $this->get_name(), $id, $type, $subtype );
 	}
 
 	/**
-	 * Generate the item options for the relationship field.
+	 * Get post options
 	 *
-	 * @return array $options The selectable options of the relationship field.
+	 * @return array $options
+	 */
+	protected function get_post_options( $type ) {
+		/**
+		 * Filter the default query when fetching posts for a particular field.
+		 *
+		 * @param array $args The parameters, passed to get_posts().
+		 */
+		$filter_name = 'carbon_association_options_' . $this->get_name() . '_' . $type['type'] . '_' . $type['post_type'];
+		$args = apply_filters( $filter_name, array(
+			'post_type' => $type['post_type'],
+			'posts_per_page' => -1,
+			'fields' => 'ids',
+			'suppress_filters' => false,
+		) );
+
+		// fetch and prepare posts as association items
+		$posts = get_posts( $args );
+		foreach ( $posts as &$p ) {
+			$p = array(
+				'id' => $p,
+				'title' => $this->get_title_by_type( $p, $type['type'], $type['post_type'] ),
+				'type' => $type['type'],
+				'subtype' => $type['post_type'],
+				'label' => $this->get_item_label( $p, $type['type'], $type['post_type'] ),
+				'is_trashed' => ( get_post_status( $p ) == 'trash' ),
+				'edit_link' => $this->get_object_edit_link( $type, $p ),
+			);
+		}
+		return $posts;
+	}
+
+	/**
+	 * Get term options
+	 *
+	 * @return array $options
+	 */
+	protected function get_term_options( $type ) {
+		/**
+		 * Filter the default parameters when fetching terms for a particular field.
+		 *
+		 * @param array $args The parameters, passed to get_terms().
+		 */
+		$filter_name = 'carbon_association_options_' . $this->get_name() . '_' . $type['type'] . '_' . $type['taxonomy'];
+		$args = apply_filters( $filter_name, array(
+			'hide_empty' => 0,
+			'fields' => 'id=>name',
+		) );
+
+		// fetch and prepare terms as association items
+		$terms = get_terms( $type['taxonomy'], $args );
+		foreach ( $terms as $term_id => &$term ) {
+			$term = array(
+				'id' => $term_id,
+				'title' => $term,
+				'type' => $type['type'],
+				'subtype' => $type['taxonomy'],
+				'label' => $this->get_item_label( $term_id, $type['type'], $type['taxonomy'] ),
+				'is_trashed' => false,
+				'edit_link' => $this->get_object_edit_link( $type, $term_id ),
+			);
+		}
+		return $terms;
+	}
+
+	/**
+	 * Get user options
+	 *
+	 * @return array $options
+	 */
+	protected function get_user_options( $type ) {
+		/**
+		 * Filter the default parameters when fetching users for a particular field.
+		 *
+		 * @param array $args The parameters, passed to get_users().
+		 */
+		$filter_name = 'carbon_association_options_' . $this->get_name() . '_' . $type['type'];
+		$args = apply_filters( $filter_name, array(
+			'fields' => 'ID',
+		) );
+
+		// fetch and prepare users as association items
+		$users = get_users( $args );
+		foreach ( $users as &$u ) {
+			$u = array(
+				'id' => $u,
+				'title' => $this->get_title_by_type( $u, $type['type'] ),
+				'type' => $type['type'],
+				'subtype' => 'user',
+				'label' => $this->get_item_label( $u, $type['type'] ),
+				'is_trashed' => false,
+				'edit_link' => $this->get_object_edit_link( $type, $u ),
+			);
+		}
+		return $users;
+	}
+
+	/**
+	 * Get comment options
+	 *
+	 * @return array $options
+	 */
+	protected function get_comment_options( $type ) {
+		/**
+		 * Filter the default parameters when fetching comments for a particular field.
+		 *
+		 * @param array $args The parameters, passed to get_comments().
+		 */
+		$filter_name = 'carbon_association_options_' . $this->get_name() . '_' . $type['type'];
+		$args = apply_filters( $filter_name, array(
+			'fields' => 'ids',
+		) );
+
+		// fetch and prepare comments as association items
+		$comments = get_comments( $args );
+		foreach ( $comments as &$c ) {
+			$c = array(
+				'id' => $c,
+				'title' => $this->get_title_by_type( $c, $type['type'] ),
+				'type' => $type['type'],
+				'subtype' => 'comment',
+				'label' => $this->get_item_label( $c, $type['type'] ),
+				'is_trashed' => false,
+				'edit_link' => $this->get_object_edit_link( $type, $c ),
+			);
+		}
+		return $comments;
+	}
+
+	/**
+	 * Generate the item options.
+	 *
+	 * @return array $options The selectable options of the association field.
 	 */
 	public function get_options() {
 		$options = array();
 
 		foreach ( $this->types as $type ) {
-
-			if ( $type['type'] === 'post' ) { // populate posts
-
-				/**
-				 * Filter the default query when fetching posts for a particular field.
-				 *
-				 * @param array $args The parameters, passed to get_posts().
-				 */
-				$filter_name = 'carbon_relationship_options_' . $this->get_name() . '_' . $type['type'] . '_' . $type['post_type'];
-				$args = apply_filters( $filter_name, array(
-					'post_type' => $type['post_type'],
-					'posts_per_page' => -1,
-					'fields' => 'ids',
-					'suppress_filters' => false,
-				) );
-
-				// fetch and prepare posts as relationship items
-				$posts = get_posts( $args );
-				foreach ( $posts as &$p ) {
-					$p = array(
-						'id' => $p,
-						'title' => $this->get_title_by_type( $p, $type['type'], $type['post_type'] ),
-						'type' => $type['type'],
-						'subtype' => $type['post_type'],
-						'label' => $this->get_item_label( $p, $type['type'], $type['post_type'] ),
-						'is_trashed' => ( get_post_status( $p ) == 'trash' ),
-						'edit_link' => $this->get_object_edit_link( $type, $p ),
-					);
-				}
-				$options = array_merge( $options, $posts );
-
-			} elseif ( $type['type'] === 'term' ) { // populate taxonomy terms
-
-				/**
-				 * Filter the default parameters when fetching terms for a particular field.
-				 *
-				 * @param array $args The parameters, passed to get_terms().
-				 */
-				$filter_name = 'carbon_relationship_options_' . $this->get_name() . '_' . $type['type'] . '_' . $type['taxonomy'];
-				$args = apply_filters( $filter_name, array(
-					'hide_empty' => 0,
-					'fields' => 'id=>name',
-				) );
-
-				// fetch and prepare terms as relationship items
-				$terms = get_terms( $type['taxonomy'], $args );
-				foreach ( $terms as $term_id => &$term ) {
-					$term = array(
-						'id' => $term_id,
-						'title' => $term,
-						'type' => $type['type'],
-						'subtype' => $type['taxonomy'],
-						'label' => $this->get_item_label( $term_id, $type['type'], $type['taxonomy'] ),
-						'is_trashed' => false,
-						'edit_link' => $this->get_object_edit_link( $type, $term_id ),
-					);
-				}
-				$options = array_merge( $options, $terms );
-
-			} elseif ( $type['type'] === 'user' ) { // populate users
-
-				/**
-				 * Filter the default parameters when fetching users for a particular field.
-				 *
-				 * @param array $args The parameters, passed to get_users().
-				 */
-				$filter_name = 'carbon_relationship_options_' . $this->get_name() . '_' . $type['type'];
-				$args = apply_filters( $filter_name, array(
-					'fields' => 'ID',
-				) );
-
-				// fetch and prepare users as relationship items
-				$users = get_users( $args );
-				foreach ( $users as &$u ) {
-					$u = array(
-						'id' => $u,
-						'title' => $this->get_title_by_type( $u, $type['type'] ),
-						'type' => $type['type'],
-						'subtype' => 'user',
-						'label' => $this->get_item_label( $u, $type['type'] ),
-						'is_trashed' => false,
-						'edit_link' => $this->get_object_edit_link( $type, $u ),
-					);
-				}
-				$options = array_merge( $options, $users );
-
-			} elseif ( $type['type'] === 'comment' ) { // populate comments
-
-				/**
-				 * Filter the default parameters when fetching comments for a particular field.
-				 *
-				 * @param array $args The parameters, passed to get_comments().
-				 */
-				$filter_name = 'carbon_relationship_options_' . $this->get_name() . '_' . $type['type'];
-				$args = apply_filters( $filter_name, array(
-					'fields' => 'ids',
-				) );
-
-				// fetch and prepare comments as relationship items
-				$comments = get_comments( $args );
-				foreach ( $comments as &$c ) {
-					$c = array(
-						'id' => $c,
-						'title' => $this->get_title_by_type( $c, $type['type'] ),
-						'type' => $type['type'],
-						'subtype' => 'comment',
-						'label' => $this->get_item_label( $c, $type['type'] ),
-						'is_trashed' => false,
-						'edit_link' => $this->get_object_edit_link( $type, $c ),
-					);
-				}
-				$options = array_merge( $options, $comments );
-
+			$method = 'get_' . $type['type'] . '_options';
+			$callable = array( $this, $method );
+			if ( is_callable( $callable ) ) {
+				$options = array_merge( $options, call_user_func( $callable, $type ) );
 			}
 		}
 
 		/**
-		 * Filter the final list of options, available to a certain relationship field.
+		 * Filter the final list of options, available to a certain association field.
 		 *
 		 * @param array $options Unfiltered options items.
-		 * @param string $name Name of the relationship field.
+		 * @param string $name Name of the association field.
 		 */
-		$options = apply_filters( 'carbon_relationship_options', $options, $this->get_name() );
+		$options = apply_filters( 'carbon_association_options', $options, $this->get_name() );
 
 		return $options;
 	}
@@ -409,7 +432,7 @@ class Association_Field extends Relationship_Field {
 	}
 
 	/**
-	 * Serves as a backbone template for the relationship items.
+	 * Serves as a backbone template for the association items.
 	 * Used for both the selected and the selectable options.
 	 *
 	 * @param bool $display_input Whether to display the selected item input field.
