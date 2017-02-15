27b9d4dedf6c563959b1a95bb59bbe7d26c1dd8a
Viktor Vasilev <viktor.2create@gmail.com>
2017-02-15 16:56:20 +0200

    Add 'Nav_Menu' container

--- assets/react/registrar.js
+++ assets/react/registrar.js
@@ -7,6 +7,7 @@ import { registerContainerComponent, registerFieldComponent } from 'lib/registry
  * Register the containers.
  */
 import CommentMetaContainer from 'containers/components/comment-meta-container';
+import NavMenuContainer from 'containers/components/nav-menu';
 import PostMetaContainer from 'containers/components/post-meta-container';
 import TermMetaContainer from 'containers/components/term-meta-container';
 import ThemeOptionsContainer from 'containers/components/theme-options-container';
@@ -15,6 +16,7 @@ import WidgetContainer from 'containers/components/widget';
 import * as ContainerConstants from 'containers/constants';
 
 registerContainerComponent(ContainerConstants.TYPE_COMMENT_META, CommentMetaContainer);
+registerContainerComponent(ContainerConstants.TYPE_NAV_MENU, NavMenuContainer);
 registerContainerComponent(ContainerConstants.TYPE_POST_META, PostMetaContainer);
 registerContainerComponent(ContainerConstants.TYPE_TERM_META, TermMetaContainer);
 registerContainerComponent(ContainerConstants.TYPE_THEME_OPTIONS, ThemeOptionsContainer);
