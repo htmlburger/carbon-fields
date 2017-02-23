d421af0eb4c30af9dc11965d642d4328571a2b5b
Viktor Vasilev <viktor.2create@gmail.com>
2017-02-23 17:54:42 +0200

    Reuse new tasks helper in containers

--- assets/react/containers/helpers.js
+++ assets/react/containers/helpers.js
@@ -1,29 +1,21 @@
 /**
  * The external dependencies.
  */
-import { take, cancel } from 'redux-saga/effects';
+import { call } from 'redux-saga/effects';
 
 /**
  * The internal dependencies.
  */
+import { cancelTasks } from 'lib/helpers';
 import { teardownContainer } from 'containers/actions';
 
 /**
- * Clear any started tasks when the container is being removed.
+ * Kill the saga.
  *
- * @param {String}   containerId
- * @param {Object[]} tasks
+ * @param  {String}   containerId
+ * @param  {Object[]} tasks
  * @return {void}
  */
 export function* stopSaga(containerId, tasks) {
-	while (true) {
-		const { payload } = yield take(teardownContainer);
-
-		// Clean up only for the specified container.
-		if (payload.containerId === containerId) {
-			yield tasks.map(task => cancel(task));
-
-			break;
-		}
-	}
+	yield call(cancelTasks, teardownContainer, tasks, ({ payload }) => payload.containerId === containerId);
 }
