de2a440ad08e45d74a50c31c0b1187f1d8c1bd8a
Atanas Angelov <atanas.angelov.dev@gmail.com>
2017-02-25 13:45:32 +0200

    add Post_Template_Condition

---
+++ core/Container/Condition/Post_Template_Condition.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Carbon_Fields\Container\Condition;
+
+use Carbon_Fields\App;
+
+/**
+ * Pass "default" as the value for the default post template
+ */
+class Post_Template_Condition extends Condition {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		$this->set_comparers( array( 
+			App::resolve( 'container_condition_comparer_type_equality' ),
+			App::resolve( 'container_condition_comparer_type_contain' ),
+			App::resolve( 'container_condition_comparer_type_regex' ),
+			App::resolve( 'container_condition_comparer_type_custom' ),
+		) );
+	}
+	
+	/**
+	 * Check if the condition is fulfilled
+	 * 
+	 * @param  array $environment
+	 * @return bool
+	 */
+	public function is_fulfilled( $environment ) {
+		$post = $environment['post'];
+		$post_id = is_object( $post ) ? intval( $post->ID ) : 0;
+		$post_template = get_post_meta( $post_id, '_wp_page_template', true );
+		$post_template = $post_template ? $post_template : 'default';
+
+		return $this->first_supported_comparer_is_correct(
+			$post_template,
+			$this->get_comparison_operator(),
+			$this->get_value()
+		);
+	}
+}
