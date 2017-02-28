c89383c44cba0d1746f02f1f7b5ad251844844cf
atanas-angelov-dev <atanas.angelov.dev@gmail.com>
2017-02-28 11:35:01 +0200

    reduce duplication in user id condition

--- core/Container/Condition/Current_User_Capability_Condition.php
+++ core/Container/Condition/Current_User_Capability_Condition.php
@@ -7,49 +7,15 @@ namespace Carbon_Fields\Container\Condition;
  * 
  * Operator "CUSTOM" is passed the user id
  */
-class Current_User_Capability_Condition extends Condition {
+class Current_User_Capability_Condition extends User_Capability_Condition {
 
 	/**
-	 * Check if a user has any of the supplied capabilities
+	 * Get user id from environment
 	 * 
-	 * @param  array<string> $capabilities
-	 * @return boolean
+	 * @param  array         $environment
+	 * @return array<string>
 	 */
-	protected function current_user_can_any( $capabilities ) {
-		foreach ( $capabilities as $cap ) {
-			if ( current_user_can( $cap ) ) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	/**
-	 * Check if the condition is fulfilled
-	 * 
-	 * @param  array $environment
-	 * @return bool
-	 */
-	public function is_fulfilled( $environment ) {
-		switch ( $this->get_comparison_operator() ) {
-			case '=':
-				return current_user_can( $this->get_value() );
-				break;
-			case '!=':
-				return ! current_user_can( $this->get_value() );
-				break;
-			case 'IN':
-				return $this->current_user_can_any( $this->get_value() );
-				break;
-			case 'NOT IN':
-				return ! $this->current_user_can_any( $this->get_value() );
-				break;
-		}
-
-		return $this->first_supported_comparer_is_correct(
-			get_current_user_id(),
-			$this->get_comparison_operator(),
-			$this->get_value()
-		);
+	protected function get_user_id( $environment ) {
+		return get_current_user_id();
 	}
 }
