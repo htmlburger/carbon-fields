de1e7732831d78dda3aa0fc444ca0ce4b7e607c7
atanas-angelov-dev <atanas.angelov.dev@gmail.com>
2017-02-28 11:31:16 +0200

    reduce duplication between role conditions

--- core/Container/Condition/User_Role_Condition.php
+++ core/Container/Condition/User_Role_Condition.php
@@ -8,6 +8,16 @@ namespace Carbon_Fields\Container\Condition;
  * Operator "CUSTOM" is passed an array of all user roles
  */
 class User_Role_Condition extends Condition {
+
+	/**
+	 * Get roles for a user from the environment
+	 * 
+	 * @param  array         $environment
+	 * @return array<string>
+	 */
+	protected function get_user_roles( $environment ) {
+		return $environment['roles'];
+	}
 	
 	/**
 	 * Check if the condition is fulfilled
@@ -16,7 +26,7 @@ class User_Role_Condition extends Condition {
 	 * @return bool
 	 */
 	public function is_fulfilled( $environment ) {
-		$roles = $environment['roles'];
+		$roles = $this->get_user_roles( $environment );
 		
 		switch ( $this->get_comparison_operator() ) {
 			case '=':
