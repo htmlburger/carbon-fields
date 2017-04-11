15c6f74d708889e601b69c856908c91125f18278
Viktor Vasilev <viktor.2create@gmail.com>
2017-04-11 17:52:57 +0300

    Upgrade postcss & rename CSS folder

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,12 @@
 module.exports = {
-	"use": ["autoprefixer", "postcss-import", "postcss-simple-vars", "cssnano"],
-	"input": "assets/postcss/main.css",
-	"output": "assets/bundle.css",
-	"autoprefixer": {
-		"browsers": "> 5%"
-	},
-	"postcss-import": {
-		onImport: function(sources) {
-			global.watchCSS( sources );
-		}
-	},
-	"cssnano": {
-		zindex: false
-	}
+	plugins: [
+		require('autoprefixer')({
+			browsers: '> 5%',
+		}),
+		require('postcss-import'),
+		require('postcss-simple-vars'),
+		require('cssnano')({
+			zindex: false,
+		}),
+	]
 };
