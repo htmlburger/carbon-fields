4bf7d9a644edaf682707b33cc61ca016a7af560c
Viktor Vasilev <viktor.2create@gmail.com>
2017-04-27 14:27:27 +0300

    Add single dev & build tasks

--- webpack.core.js
+++ webpack.core.js
@@ -1,16 +1,14 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
+const execa = require('execa');
 const base = require('./webpack.base');
+const OnBuildPlugin = require('on-build-webpack');
 
-module.exports = (env) => merge(base(env), {
-	entry: {
-		'carbon.core': [
-			'./assets/js/core.js',
-		]
-	},
+module.exports = (env) => {
+	let bootstraped = false;
 
-	plugins: [
+	const plugins = [
 		new webpack.DllReferencePlugin({
 			manifest: require(base.getNameWithSuffix('./assets/dist/carbon.vendor.json', env)),
 			sourceType: 'this'
@@ -20,5 +18,27 @@ module.exports = (env) => merge(base(env), {
 			name: '[name]',
 			path: path.resolve(__dirname, base.getNameWithSuffix('assets/dist/[name].json', env))
 		})
-	]
-});
+	];
+
+	if (!env || env === 'development') {
+		plugins.push(new OnBuildPlugin(() => {
+			if (!bootstraped) {
+				bootstraped = true;
+				const child = execa('npm', ['run', 'js:boot', '--', '-w']);
+
+				child.stdout.pipe(process.stdout);
+				child.stderr.pipe(process.stderr);
+			}
+		}));
+	}
+
+	return merge(base(env), {
+		entry: {
+			'carbon.core': [
+				'./assets/js/core.js',
+			]
+		},
+
+		plugins,
+	});
+};
