5046dbe31b3f4b80fb4ab3df067bdef9d4895d8f
atanas-angelov-dev <atanas.angelov.dev@gmail.com>
2017-05-29 16:17:22 +0300

    fix user meta container title remaining visible at all times

--- templates/Container/user_meta.php
+++ templates/Container/user_meta.php
@@ -1,12 +1,10 @@
-<div id="<?php echo $this->id; ?>-wrapper">
+<div id="<?php echo $this->id; ?>">
 	<h3><?php echo $this->title; ?></h3>
 	<table class="form-table">
 		<tr class="carbon-table-row">
 			<th></th>
 			<td>
-				<div id="<?php echo $this->id; ?>">
-					<fieldset class="container-holder carbon-user-container container-<?php echo $this->id; ?> <?php echo $this->is_tabbed() ? '' : 'carbon-fields-collection' ?>" data-profile-role="<?php echo $profile_role ?>"></fieldset>
-				</div>
+				<fieldset class="container-holder carbon-user-container container-<?php echo $this->id; ?> <?php echo $this->is_tabbed() ? '' : 'carbon-fields-collection' ?>" data-profile-role="<?php echo $profile_role ?>"></fieldset>
 			</td>
 		</tr>
 	</table>
