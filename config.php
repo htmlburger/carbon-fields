1ffd9671e82aa5ec69d9a43311b3c18aad457081
atanas-angelov-dev <atanas.angelov.dev@gmail.com>
2017-07-18 15:05:15 +0300

    bump version

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ namespace Carbon_Fields;
 
 # Define version constant
 if ( ! defined( __NAMESPACE__ . '\VERSION' ) ) {
-	define( __NAMESPACE__ . '\VERSION', '2.0.1' );
+	define( __NAMESPACE__ . '\VERSION', '2.0.3' );
 }
 
 # Define root directory
