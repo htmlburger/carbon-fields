82be5f0da6ee5cf7540dad1766b447b0ca56af19
niko <sotim@abv.bg>
2017-11-04 15:36:17 +0200

    Handle situation when the site is not multisite

--- core/Container/Network_Container.php
+++ core/Container/Network_Container.php
@@ -18,25 +18,26 @@ class Network_Container extends Theme_Options_Container {
 	protected $site_id;
 
 	public function __construct( $id, $title, $type, $condition_collection, $condition_translator ) {
+		parent::__construct( $id, $title, $type, $condition_collection, $condition_translator );
+
 		if( !is_multisite() ) {
 			Incorrect_Syntax_Exception::raise( 'The ' . $title. ' is unavailable because your site is not multisite. ' );
 			return ;
 		}
 
-		parent::__construct( $id, $title, $type, $condition_collection, $condition_translator );
 
 		$this->set_datastore( Datastore::make( 'network' ), $this->has_default_datastore() );
 
 		$this->set_site_id( SITE_ID_CURRENT_SITE );
 	}
-
+	
 	public function init() {
 		$registered = $this->register_page();
 		if ( $registered ) {
 			add_action( 'network_admin_menu', array( $this, '_attach' ) );
 		}
 	}
-
+	
 	public function render() {
 		$input = stripslashes_deep( $_GET );
 		$request_settings_updated = isset( $input['settings-updated'] ) ? $input['settings-updated'] : '';
@@ -48,8 +49,15 @@ class Network_Container extends Theme_Options_Container {
 	}
 
 	public function set_site_id( $id ) {
+		if( !function_exists( 'get_blog_status' ) ) {
+			return $this;
+		}
+
 		if( $this->site_exists( $id ) ) {
 			$this->site_id = $id;
+		} else {
+			Incorrect_Syntax_Exception::raise( 'The specified site id ' . $id . ' does not exist' );
+			return $this;
 		}
 
 		$this->datastore->set_object_id( $this->get_site_id() );
