5d7d6c3fd0145a20c8cd8bff3c1a71b9d1676b70
Viktor Vasilev <liberalcho@gmail.com>
2018-10-01 17:50:35 +0300

    Setup datastore of block container

--- core/Container/Block_Container.php
+++ core/Container/Block_Container.php
@@ -3,8 +3,6 @@
 namespace Carbon_Fields\Container;
 
 use Carbon_Fields\Datastore\Datastore;
-use Carbon_Fields\Helper\Helper;
-use Carbon_Fields\Exception\Incorrect_Syntax_Exception;
 
 /**
  * Theme options container class.
@@ -15,6 +13,10 @@ class Block_Container extends Container {
 	 */
 	public function __construct( $id, $title, $type, $condition_collection, $condition_translator ) {
 		parent::__construct( $id, $title, $type, $condition_collection, $condition_translator );
+
+		if ( ! $this->get_datastore() ) {
+			$this->set_datastore( Datastore::make( 'empty' ), $this->has_default_datastore() );
+		}
 	}
 
 	/**
@@ -22,7 +24,6 @@ class Block_Container extends Container {
 	 */
 	public function init() {
 		add_action( 'init', array( $this, '_attach' ) );
-		add_action( 'save_post', array( $this, '_save' ) );
 	}
 
 	/**
@@ -40,9 +41,7 @@ class Block_Container extends Container {
 	 *
 	 * @param mixed $user_data
 	 */
-	public function save( $user_data = null ) {
-
-	}
+	public function save( $user_data = null ) {}
 
 	/**
 	 * Get environment array for page request (in admin)
@@ -59,7 +58,7 @@ class Block_Container extends Container {
 	 * @return bool True if the container is allowed to be attached
 	 */
 	public function is_valid_attach_for_request() {
-		return $this->static_conditions_pass();
+		return true;
 	}
 
 	/**
@@ -78,16 +77,14 @@ class Block_Container extends Container {
 	 * @return bool
 	 */
 	public function is_valid_attach_for_object( $object_id = null ) {
-		return $this->all_conditions_pass( intval( $object_id ) );
+		return true;
 	}
 
 	/**
 	 * Add theme options container pages.
 	 * Hook the container saving action.
 	 */
-	public function attach() {
-
-	}
+	public function attach() {}
 
 	/**
 	 * Whether this container is currently viewed.
@@ -101,7 +98,5 @@ class Block_Container extends Container {
 	/**
 	 * Output the container markup
 	 */
-	public function render() {
-
-	}
+	public function render() {}
 }
