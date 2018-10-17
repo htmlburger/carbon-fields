10d34b7b46270ada21891266f7e90d47e2f87d66
Viktor Vasilev <liberalcho@gmail.com>
2018-10-17 17:50:43 +0300

    Remove absolute imports in core package

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,15 +1,15 @@
 /**
  * Internal dependencies.
  */
-import { registerFieldType } from 'lib/registry';
-import CheckboxField from 'components/checkbox';
-import ColorField from 'components/color';
-import HtmlField from 'components/html';
-import RadioField from 'components/radio';
-import RadioImageField from 'components/radio-image';
-import SelectField from 'components/select';
-import SetField from 'components/set';
-import TextField from 'components/text';
+import { registerFieldType } from './lib/registry';
+import CheckboxField from './components/checkbox';
+import ColorField from './components/color';
+import HtmlField from './components/html';
+import RadioField from './components/radio';
+import RadioImageField from './components/radio-image';
+import SelectField from './components/select';
+import SetField from './components/set';
+import TextField from './components/text';
 
 /**
  * Registers core fields.
@@ -25,4 +25,4 @@ import TextField from 'components/text';
 	[ 'text', TextField ]
 ].forEach( ( field ) => registerFieldType( ...field ) );
 
-export * from 'lib/registry';
+export * from './lib/registry';
