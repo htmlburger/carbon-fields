d468fd09384b1c31917959380608b971207f61b4
ggeorgiev96 <ggeorgiev.2create@gmail.com>
2018-10-18 16:32:11 +0300

    Implement @wordpress/i18n for file field button label
    TODO Add custom localization domain

--- packages/gutenberg/fields/file/index.js
+++ packages/gutenberg/fields/file/index.js
@@ -3,8 +3,9 @@
  */
 import { addFilter } from '@wordpress/hooks';
 import { Fragment } from '@wordpress/element';
-import { BaseControl, Button } from '@wordpress/components';
+import { BaseControl, Button, Placeholder } from '@wordpress/components';
 import { MediaUpload } from '@wordpress/editor';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Renders the field.
@@ -15,9 +16,32 @@ const FileField = ( {
 	buttonLabel,
 	field,
 	value,
-	handleSelect,
-	renderFileComponent
+	handleSelect
 } ) => {
+	/**
+	 * Provide the file component
+	 *
+	 * @return {Null|Object}
+	 */
+	const renderFileComponent = () => {
+		if ( ! value ) {
+			return null;
+		}
+
+		if ( /image/.test( value.mime ) ) {
+			return (
+				<img src={ value.url } />
+			);
+		}
+
+		return (
+			<Placeholder
+				icon="media-document"
+				label={ value.filename }
+			/>
+		);
+	};
+
 	return (
 		<BaseControl label={ field.label }>
 			<MediaUpload
@@ -39,8 +63,19 @@ const FileField = ( {
 
 addFilter( 'carbon-fields.file-field.gutenberg', 'carbon-fields/gutenberg', ( OriginalFileField ) => ( { field, value, onChange } ) => {
 	return (
-		<OriginalFileField buttonLabelProp="fileButtonLabel" field={ field } onChange={ onChange } value={ value }>
-			{ ( { ...props } ) => <FileField field={ field } value={ value } { ...props } /> }
+		<OriginalFileField
+			field={ field }
+			onChange={ onChange }
+			value={ value }
+		>
+			{ ( { ...props } ) => (
+				<FileField
+					field={ field }
+					value={ value }
+					buttonLabel={ __( 'Select File' ) }
+					{ ...props }
+				/>
+			) }
 		</OriginalFileField>
 	);
 } );
