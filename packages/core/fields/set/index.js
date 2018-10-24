14d9c2e7323c660db6efd409dd1fd9d424ad666e
Viktor Vasilev <liberalcho@gmail.com>
2018-10-24 14:40:55 +0300

    Clean up implementation of set field

--- packages/core/fields/set/index.js
+++ packages/core/fields/set/index.js
@@ -2,18 +2,47 @@
  * External dependencies.
  */
 import { Component } from '@wordpress/element';
+import { xor } from 'lodash';
 
 class SetField extends Component {
-	isChecked = ( key, value ) => {
-		return Array.isArray( value ) && value.indexOf( key ) > -1;
+	/**
+	 * Handles the change of the field.
+	 *
+	 * @param  {string} fieldKey
+	 * @param  {mixed}  value
+	 * @return {void}
+	 */
+	handleChange = ( fieldKey, value ) => {
+		this.props.onChange(
+			fieldKey,
+			xor( this.props.value, [ value ] )
+		);
 	}
+
+	/**
+	 * Checks if the given option is checked.
+	 *
+	 * @param  {Object} option
+	 * @return {boolean}
+	 */
+	isChecked = ( option ) => {
+		return this.props.value.indexOf( option.value ) > -1;
+	}
+
 	/**
 	 * Render the component.
 	 *
 	 * @return {Object}
 	 */
 	render() {
-		return this.props.children( { isChecked: this.isChecked } );
+		const { field, value } = this.props;
+
+		return this.props.children( {
+			field,
+			value,
+			isChecked: this.isChecked,
+			handleChange: this.handleChange
+		} );
 	}
 }
 
