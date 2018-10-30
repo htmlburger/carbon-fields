1b79a0a51915263f393928215bd2ddfd07db0bcb
Viktor Vasilev <liberalcho@gmail.com>
2018-10-30 15:23:54 +0200

    Implement side effects for `post_parent_id` in Post Meta container

--- packages/metaboxes/containers/post-meta/gutenberg.js
+++ packages/metaboxes/containers/post-meta/gutenberg.js
@@ -5,22 +5,38 @@ import { withSelect } from '@wordpress/data';
 import { addFilter } from '@wordpress/hooks';
 import { compose } from '@wordpress/compose';
 import { withEffects } from 'refract-callbag';
+import {
+	map,
+	pipe,
+	merge,
+	filter
+} from 'callbag-basics';
 
 /**
  * Internal dependencies.
  */
 import withContainer from '../../components/with-container';
 import handler from './handler';
+import { POST_PARENT_ID } from './constants';
 
 /**
  * The function that controls the stream of side-effects.
  *
- * @param  {Object} props
  * @return {Function}
  */
 function aperture() {
-	return function() {
+	return function( component ) {
+		const postParentId$ = pipe(
+			component.observe( 'postParentId' ),
+			map( ( postParentId ) => parseInt( postParentId, 10 ) ),
+			filter( ( postParentId ) => ! isNaN( postParentId ) ),
+			map( ( postParentId ) => ( {
+				type: POST_PARENT_ID,
+				payload: postParentId
+			} ) )
+		);
 
+		return merge( postParentId$ );
 	};
 }
 
@@ -32,7 +48,7 @@ const applyWithSelect = withSelect( ( select ) => {
 	};
 } );
 
-const applyWithEffects = withEffects( aperture )( handler );
+const applyWithEffects = withEffects( handler )( aperture );
 
 addFilter( 'carbon-fields.post_meta-container.gutenberg', 'carbon-fields/metaboxes', ( OriginalPostMetaContainer ) => compose(
 	withContainer,
--- packages/metaboxes/containers/post-meta/handler.js
+++ packages/metaboxes/containers/post-meta/handler.js
@@ -1,11 +1,19 @@
 /**
+ * Internal dependencies.
+ */
+import { POST_PARENT_ID } from './constants';
+
+/**
  * The handler causes the side effects.
  *
  * @param  {Object} props
  * @return {Function}
  */
-export default function handler() {
-	return function() {
-
+export default function handler( props ) {
+	return function( effect ) {
+		switch ( effect.type ) {
+			case POST_PARENT_ID:
+				return props.updateContainerMeta( props.id, 'post_parent_id', effect.payload );
+		}
 	};
 }
