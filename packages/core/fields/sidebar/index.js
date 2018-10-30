aab82d11ca0c00723146b45f8b12aaf911a7bd74
ggeorgiev96 <ggeorgiev.2create@gmail.com>
2018-10-30 17:38:06 +0200

    Update the sidebar core component

--- packages/core/fields/sidebar/index.js
+++ packages/core/fields/sidebar/index.js
@@ -5,12 +5,60 @@ import { Component } from '@wordpress/element';
 
 class SidebarField extends Component {
 	/**
+	 * Defines the component state
+	 *
+	 * @return {void}
+	 */
+	constructor() {
+		super();
+
+		this.state = {
+			newSidebarName: ''
+		};
+	}
+
+	/**
+	 * Handles the change of the new sidebar input
+	 *
+	 * @param  {string} value
+	 * @return {void}
+	 */
+	handleNewSidebarNameChange = ( value ) => this.setState( { newSidebarName: value } )
+
+	/**
+	 * Checks if the sidebar has 'add new' selected
+	 *
+	 * @return {boolean}
+	 */
+	canAddNew = () => {
+		const { field } = this.props;
+
+		return field.value === '__add_new' || field.options.length === 1;
+	}
+
+	/**
 	 * Render the component.
 	 *
 	 * @return {Object}
 	 */
 	render() {
-		return this.props.children();
+		const {
+			field,
+			onChange,
+			children
+		} = this.props;
+
+		if ( ! children ) {
+			return null;
+		}
+
+		return children( {
+			field: field,
+			handleChange: onChange,
+			canAddNew: this.canAddNew,
+			handleNewSidebarNameChange: this.handleNewSidebarNameChange,
+			newSidebarName: this.state.newSidebarName
+		} );
 	}
 }
 
