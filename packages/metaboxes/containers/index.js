c7d7468f0450fb109937cada5da46a6f70d18fae
Viktor Vasilev <liberalcho@gmail.com>
2018-11-06 10:39:48 +0200

    [User Meta] Add bare implementation

--- packages/metaboxes/containers/index.js
+++ packages/metaboxes/containers/index.js
@@ -4,11 +4,13 @@
 import { registerContainerType } from './registry';
 import PostMetaContainer from './post-meta';
 import TermMetaContainer from './term-meta';
+import UserMetaContainer from './user-meta';
 
 /**
  * Registers the containers.
  */
 [
 	[ 'post_meta', PostMetaContainer ],
-	[ 'term_meta', TermMetaContainer ]
+	[ 'term_meta', TermMetaContainer ],
+	[ 'user_meta', UserMetaContainer ]
 ].forEach( ( container ) => registerContainerType( ...container ) );
