17ec6c66eccb716f78be48922b2c5e829ee00938
Viktor Vasilev <liberalcho@gmail.com>
2018-11-06 16:23:39 +0200

    Refactor logic that flattens the fields

--- packages/metaboxes/utils/flatten-field.js
+++ packages/metaboxes/utils/flatten-field.js
@@ -1,13 +1,14 @@
 /**
  * External dependencies.
  */
-import { assign, pick } from 'lodash';
+import {
+	pick,
+	uniqueId,
+	cloneDeep
+} from 'lodash';
 
 /**
  * Flattens a field.
- *
- * TODO:
- * - Handle complex fields.
  *
  * @param  {Object}   field
  * @param  {string}   containerId
@@ -15,11 +16,25 @@ import { assign, pick } from 'lodash';
  * @return {Object}
  */
 export default function flattenField( field, containerId, accumulator ) {
+	field = cloneDeep( field );
+
+	// Replace the id of the field.
+	field.id = uniqueId( 'carbon-fields-' );
+
+	// Keep reference to the container.
 	field.container_id = containerId;
 
-	accumulator.push( assign( {}, field, {
-		container_id: containerId
-	} ) );
+	// The complex fields represent a nested structure of fields.
+	// So we need to flat them as well.
+	if ( field.type === 'complex' ) {
+		field.value.forEach( ( group ) => {
+			group.id = uniqueId( 'carbon-fields-' );
+			group.container_id = containerId;
+			group.fields = group.fields.map( ( groupField ) => flattenField( groupField, containerId, accumulator ) );
+		} );
+	}
+
+	accumulator.push( field );
 
 	return pick( field, [
 		'id',
