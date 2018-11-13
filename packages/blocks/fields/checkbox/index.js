0c9c49f9410961d3c2b7ebd3fde3066d05807325
ggeorgiev96 <ggeorgiev.2create@gmail.com>
2018-11-13 15:35:31 +0200

    [Checkbox] Move rendering to core component

--- packages/blocks/fields/checkbox/index.js
+++ packages/blocks/fields/checkbox/index.js
@@ -1,57 +1,6 @@
 /**
  * External dependencies.
  */
-import { Component } from '@wordpress/element';
-import { CheckboxControl } from '@wordpress/components';
 import { addFilter } from '@wordpress/hooks';
 
-class CheckboxField extends Component {
-	/**
-	 * Handles the change of the checkbox.
-	 *
-	 * @param  {string} value
-	 * @return {void}
-	 */
-	handleChange = ( value ) => {
-		const { name, onChange } = this.props;
-
-		onChange( name, value );
-	}
-
-	/**
-	 * Renders the component.
-	 *
-	 * @return {Object}
-	 */
-	render() {
-		const { field, value } = this.props;
-
-		return (
-			<CheckboxControl
-				label={ field.option_label }
-				checked={ value }
-				onChange={ this.handleChange }
-			/>
-		);
-	}
-}
-
-addFilter( 'carbon-fields.checkbox-field.block', 'carbon-fields/blocks', ( OriginalCheckboxField ) => ( props ) => {
-	return (
-		<OriginalCheckboxField { ...props }>
-			{ ( {
-				field,
-				name,
-				value,
-				handleChange
-			} ) => (
-				<CheckboxField
-					field={ field }
-					name={ name }
-					value={ value }
-					onChange={ handleChange }
-				/>
-			) }
-		</OriginalCheckboxField>
-	);
-} );
+addFilter( 'carbon-fields.checkbox-field.block', 'carbon-fields/blocks', ( OriginalCheckboxField ) => OriginalCheckboxField );
