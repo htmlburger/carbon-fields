04fb1fe2d53e4061b4227608952a6226946bc5b6
ggeorgiev96 <ggeorgiev.2create@gmail.com>
2018-11-13 15:31:35 +0200

    [Textarea] Move rendering to core component

--- packages/blocks/fields/textarea/index.js
+++ packages/blocks/fields/textarea/index.js
@@ -1,61 +1,6 @@
 /**
  * External dependencies.
  */
-import { Component } from '@wordpress/element';
-import { RichText } from '@wordpress/editor';
-import { BaseControl } from '@wordpress/components';
 import { addFilter } from '@wordpress/hooks';
 
-class TextareaField extends Component {
-	/**
-	 * Handles the change of the textare.
-	 *
-	 * @param  {string} value
-	 * @return {void}
-	 */
-	handleChange = ( value ) => {
-		const { name, onChange } = this.props;
-
-		onChange( name, value );
-	}
-
-	/**
-	 * Renders the component.
-	 *
-	 * @return {Object}
-	 */
-	render() {
-		const { field, value } = this.props;
-
-		return (
-			<BaseControl label={ field.label }>
-				<RichText
-					value={ value }
-					formattingControls={ [] }
-					multiline
-					onChange={ this.handleChange }
-				/>
-			</BaseControl>
-		);
-	}
-}
-
-addFilter( 'carbon-fields.textarea-field.block', 'carbon-fields/blocks', ( OriginalTextareaField ) => ( props ) => {
-	return (
-		<OriginalTextareaField { ...props }>
-			{ ( {
-				field,
-				name,
-				value,
-				handleChange
-			} ) => (
-				<TextareaField
-					field={ field }
-					name={ name }
-					value={ value }
-					onChange={ handleChange }
-				/>
-			) }
-		</OriginalTextareaField>
-	);
-} );
+addFilter( 'carbon-fields.textarea-field.block', 'carbon-fields/blocks', ( OriginalTextareaField ) => OriginalTextareaField );
