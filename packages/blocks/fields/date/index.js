b28abaf88dbdb5ac30e21e740094f6747eb844b5
ggeorgiev96 <ggeorgiev.2create@gmail.com>
2018-11-13 12:44:06 +0200

    Add date-time field implementation - gutenberg contetxt
    Rework core implementation of field - move render to the original container
    Refactor metabox implementation - reflect the core changes

---
+++ packages/blocks/fields/date/index.js
@@ -0,0 +1,18 @@
+/**
+ * External dependencies.
+ */
+import { Fragment } from '@wordpress/element';
+import { addFilter } from '@wordpress/hooks';
+import { __ } from '@wordpress/i18n';
+
+addFilter( 'carbon-fields.date-field.block', 'carbon-fields/blocks', ( OriginalDatetimeField ) => ( props ) => {
+	return (
+		<Fragment>
+			<OriginalDatetimeField
+				fieldKey={ props.name }
+				buttonText={ __( 'Select Date' ) }
+				{ ...props }
+			/>
+		</Fragment>
+	);
+} );
--- packages/core/fields/datetime/index.js
+++ packages/core/fields/datetime/index.js
@@ -2,8 +2,87 @@
  * External dependencies.
  */
 import { Component } from '@wordpress/element';
+import Flatpickr from 'react-flatpickr';
+
+/**
+ * The internal dependencies.
+ */
+import './style.scss';
 
 class DatetimeField extends Component {
+	/**
+	 * Handles the intialization of the flatpickr component.
+	 *
+	 * @param  {Object} selectedDateObject
+	 * @param  {string} selectedDateString
+	 * @param  {Object} instance
+	 * @return {void}
+	 */
+	handleReady = (
+		[ selectedDateObject ], // eslint-disable-line no-unused-vars
+		selectedDateString = '', // eslint-disable-line no-unused-vars
+		instance
+	) => {
+		this.props.field.picker = instance;
+	}
+
+	/**
+	 * Handles the blur event of the date input element.
+	 *
+	 * @param  {Object} e
+	 * @param  {string} fieldKey
+	 * @return {void}
+	 */
+	handleManualInput = ( e ) => {
+		const {
+			field,
+			fieldKey,
+			onChange
+		} = this.props;
+
+		const value = e.target.value;
+
+		if ( value !== field.value ) {
+			onChange( fieldKey, value );
+		}
+	}
+
+	/**
+	 * Handles the blur event of the date input element.
+	 *
+	 * @param  {Object} e
+	 * @return {void}
+	 */
+	formatManualInput = ( e ) => {
+		const value = e.target.value;
+
+		this.props.field.picker.setDate( value, true );
+	}
+
+	/**
+	 * Handles the change of the flatpickr component.
+	 *
+	 * @param  {Object} selectedDateObject
+	 * @param  {string} selectedDateString
+	 * @param  {Object} instance
+	 * @return {void}
+	 */
+	handleChange = (
+		[ selectedDateObject ], // eslint-disable-line no-unused-vars
+		selectedDateString = '',
+		instance // eslint-disable-line no-unused-vars
+	) => {
+		const {
+			field,
+			fieldKey,
+			onChange
+		} = this.props;
+
+		if ( selectedDateString !== field.value ) {
+			onChange( fieldKey, selectedDateString );
+		}
+	}
+
 	/**
 	 * Render the component.
 	 *
@@ -14,16 +93,40 @@ class DatetimeField extends Component {
 			field,
 			name,
 			value,
-			children,
-			onChange
+			buttonText
 		} = this.props;
 
-		return children( {
-			field,
-			name,
-			value,
-			handleChange: onChange
-		} );
+		return (
+			<Flatpickr
+				options={ {
+					...field.picker_options,
+					wrap: true
+				} }
+				value={ value }
+				onReady={ this.handleReady }
+				onChange={ this.handleChange }
+				className="carbon-field-group-holder"
+			>
+				<input
+					type="text"
+					name={ name }
+					value={ value }
+					onChange={ this.handleManualInput }
+					onBlur={ this.formatManualInput }
+					className="regular-text carbon-field-group-input"
+					data-input
+					{ ...field.attributes }
+				/>
+
+				<button
+					type="button"
+					className="button"
+					data-toggle
+				>
+					{ buttonText }
+				</button>
+			</Flatpickr>
+		);
 	}
 }
 
