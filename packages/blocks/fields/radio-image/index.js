c96b3aa6e6a73dd3ffd56e97afa7b0e8f9368e23
ggeorgiev96 <ggeorgiev.2create@gmail.com>
2018-11-14 10:39:23 +0200

    [Radio/Radio-image] Move rendering to core component

--- packages/blocks/fields/radio-image/index.js
+++ packages/blocks/fields/radio-image/index.js
@@ -3,31 +3,17 @@
  */
 import { addFilter } from '@wordpress/hooks';
 
-/**
- * The internal dependencies.
- */
-import { RadioField } from '../radio';
-
 addFilter( 'carbon-fields.radio_image-field.block', 'carbon-fields/blocks', ( OriginalRadioField ) => ( props ) => {
 	return (
-		<OriginalRadioField { ...props }>
-			{ ( {
-				field,
-				name,
-				value,
-				handleChange
-			} ) => (
-				<RadioField
-					name={ name }
-					field={ field }
-					value={ value }
-					options={ field.options.map( ( option ) => ( {
-						value: option.value,
-						label: ( <img src={ option.label } /> )
-					} ) ) }
-					onChange={ handleChange }
-				/>
-			) }
-		</OriginalRadioField>
+		<OriginalRadioField
+			{ ...props }
+			field={ {
+				...props.field,
+				options: props.field.options.map( ( option ) => ( {
+					...option,
+					label: ( <img src={ option.label } /> )
+				} ) )
+			} }
+		/>
 	);
 } );
--- packages/metaboxes/fields/radio-image/index.js
+++ packages/metaboxes/fields/radio-image/index.js
@@ -7,30 +7,18 @@ import { addFilter } from '@wordpress/hooks';
  * The internal dependencies.
  */
 import withField from '../../components/with-field';
-import { RadioField } from '../radio';
 
-addFilter( 'carbon-fields.radio_image-field.metabox', 'carbon-fields/metaboxes', ( OriginalRadioImageField ) => withField( ( props ) => {
+addFilter( 'carbon-fields.radio_image-field.metabox', 'carbon-fields/metaboxes', ( OriginalRadioField ) => withField( ( props ) => {
 	return (
-		<OriginalRadioImageField { ...props }>
-			{ ( {
-				field,
-				name,
-				value,
-				handleChange
-			} ) => (
-				<RadioField
-					field={ {
-						...field,
-						options: field.options.map( ( option ) => ( {
-							...option,
-							label: ( <img src={ option.label } /> )
-						} ) )
-					} }
-					name={ name }
-					value={ value }
-					onChange={ handleChange }
-				/>
-			) }
-		</OriginalRadioImageField>
+		<OriginalRadioField
+			{ ...props }
+			field={ {
+				...props.field,
+				options: props.field.options.map( ( option ) => ( {
+					...option,
+					label: ( <img src={ option.label } /> )
+				} ) )
+			} }
+		/>
 	);
 } ) );
