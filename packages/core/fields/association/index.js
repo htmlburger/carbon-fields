3e6f8172b5fa36ae621311b4673885a5ac3d4342
jorostoyanov <stoyanov.gs@gmail.com>
2018-11-14 18:22:04 +0200

    [Association] Move render to core

--- packages/core/fields/association/index.js
+++ packages/core/fields/association/index.js
@@ -1,24 +1,28 @@
 /**
  * External dependencies.
  */
+import { Component } from '@wordpress/element';
+import { compose, withState } from '@wordpress/compose';
 import { withEffects, toProps } from 'refract-callbag';
+import cx from 'classnames';
 import {
+	cloneDeep,
 	debounce,
 	find,
-	isMatch
+	isMatch,
+	without
 } from 'lodash';
 import {
 	map,
-	pipe,
-	merge
+	merge,
+	pipe
 } from 'callbag-basics';
 import of from 'callbag-of';
 
 /**
- * WordPress dependencies.
+ * Internal dependencies.
  */
-import { Component } from '@wordpress/element';
-import { compose, withState } from '@wordpress/compose';
+import FieldBase from '../../components/field-base';
 
 class AssociationField extends Component {
 	handleQueryTermChange = debounce( ( queryTerm ) => {
@@ -35,6 +39,54 @@ class AssociationField extends Component {
 			queryTerm: queryTerm
 		} );
 	}, 300 );
+
+	/**
+	 * Handles addition of a new item.
+	 *
+	 * @param  {Array} option
+	 * @return {void}
+	 */
+	handleAddItem = ( option ) => {
+		const {
+			field,
+			id,
+			value,
+			onChange
+		} = this.props;
+
+		// Don't do anything if the duplicates aren't allowed and
+		// the item is already selected.
+		if ( ! field.duplicates_allowed && option.disabled ) {
+			return;
+		}
+
+		// Don't do anything, because the maximum is reached.
+		if ( field.max > 0 && value.length >= field.max ) {
+			// alert( carbonFieldsL10n.field.maxNumItemsReached.replace( '%s', field.max ) );
+			return;
+		}
+
+		onChange( id, [
+			...value,
+			cloneDeep( option )
+		] );
+	}
+
+	/**
+	 * Handles addition of a new item.
+	 *
+	 * @param  {Array} option
+	 * @return {void}
+	 */
+	handleRemoveItem = ( option ) => {
+		const {
+			value,
+			id,
+			onChange
+		} = this.props;
+
+		onChange( id, without( value, option ) );
+	}
 
 	componentDidMount() {
 		const {
@@ -59,7 +111,7 @@ class AssociationField extends Component {
 			name,
 			value,
 			totalOptionsCount,
-			onChange
+			queryTerm
 		} = this.props;
 
 		let { options } = this.props;
@@ -76,6 +128,7 @@ class AssociationField extends Component {
 			} );
 		}
 
+		// eslint-disable-next-line
 		const sortableOptions = {
 			axis: 'y',
 			items: 'li',
@@ -85,16 +138,100 @@ class AssociationField extends Component {
 			handle: '.mobile-handle'
 		};
 
-		return this.props.children( {
-			field: field,
-			name: name,
-			value: value,
-			options: options,
-			sortableOptions: sortableOptions,
-			totalOptionsCount: totalOptionsCount,
-			handleChange: onChange,
-			handleQueryTermChange: this.handleQueryTermChange
-		} );
+		return (
+			<FieldBase
+				field={ field }
+				className="cf-field-association-wrapper"
+			>
+				<strong className="cf-field-association__counter">
+					showing { options.length } of { totalOptionsCount } results
+				</strong>
+
+				<div className="cf-field-association">
+					<div className="cf-field-association__body">
+						<div className="cf-field-association__search-bar">
+							<input
+								type="text"
+								value={ queryTerm }
+								placeholder={ carbonFieldsL10n.field.searchPlaceholder }
+								onChange={ this.handleQueryTermChange }
+							/>
+						</div>
+
+						<div className="cf-field-association__col cf-field-association__col--source">
+							{
+								options.map( ( option, index ) => {
+									return (
+										<div className={ cx( 'cf-field-association__option', { 'cf-field-association__option--selected': option.disabled } ) } key={ index }>
+											<img className="cf-field-association__option-thumbnail" src={ option.thumbnail ? option.thumbnail : '' } />
+
+											<div className="cf-field-association__option-content">
+												<span className="cf-field-association__option-title">
+													{ option.title }
+												</span>
+
+												<span className="cf-field-association__option-type">
+													{ option.type }
+												</span>
+											</div>
+
+											<div className="cf-field-association__option-actions">
+												<a href={ option.edit_link }>
+													<span className="dashicons dashicons-edit"></span> Edit
+												</a>
+
+												<button type="button" className="is-small" onClick={ () => this.handleAddItem( option ) }>
+													<span className="dashicons dashicons-plus"></span> Add
+												</button>
+											</div>
+										</div>
+									);
+								} )
+							}
+						</div>
+
+						<div className="cf-field-association__col cf-field-association__col--selected">
+							{
+								value.map( ( option, index ) => {
+									return (
+										<div className="cf-field-association__option" key={ index }>
+											<div className="cf-field-association__option-sort">
+												<span className="dashicons dashicons-menu"></span>
+											</div>
+
+											<img className="cf-field-association__option-thumbnail" src={ option.thumbnail ? option.thumbnail : '' } />
+
+											<div className="cf-field-association__option-content">
+												<span className="cf-field-association__option-title">
+													{ option.title }
+												</span>
+
+												<span className="cf-field-association__option-type">
+													{ option.type }
+												</span>
+											</div>
+
+											<div className="cf-field-association__option-actions">
+												<button type="button" onClick={ () => this.handleRemoveItem( option ) }>
+													<span className="dashicons dashicons-trash"></span> Remove
+												</button>
+											</div>
+
+											<input
+												type="hidden"
+												name={ `${ name }[${ index }]` }
+												value={ `${ option.type }:${ option.subtype }:${ option.id }` }
+												readOnly={ true }
+											/>
+										</div>
+									);
+								} )
+							}
+						</div>
+					</div>
+				</div>
+			</FieldBase>
+		);
 	}
 }
 
