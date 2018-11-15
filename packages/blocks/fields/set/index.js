4efae124d83614ec98ef531ffcdb425dcfa9a6ad
Viktor Vasilev <liberalcho@gmail.com>
2018-11-15 15:55:27 +0200

    [Set] Add basic styles

--- packages/blocks/fields/set/index.js
+++ packages/blocks/fields/set/index.js
@@ -3,4 +3,9 @@
  */
 import { addFilter } from '@wordpress/hooks';
 
+/**
+ * Internal dependencies.
+ */
+import './style.scss';
+
 addFilter( 'carbon-fields.set-field.block', 'carbon-fields/blocks', ( OriginalSetField ) => OriginalSetField );
