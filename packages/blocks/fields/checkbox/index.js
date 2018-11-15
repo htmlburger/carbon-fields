dd239d0b5412fa9626272cf1bd1a69560b122f23
Viktor Vasilev <liberalcho@gmail.com>
2018-11-15 12:16:37 +0200

    [Blocks] Remove unused styles from labels of checkable inputs

--- packages/blocks/fields/checkbox/index.js
+++ packages/blocks/fields/checkbox/index.js
@@ -3,9 +3,4 @@
  */
 import { addFilter } from '@wordpress/hooks';
 
-/**
- * Internal dependencies.
- */
-import './style.scss';
-
 addFilter( 'carbon-fields.checkbox-field.block', 'carbon-fields/blocks', ( OriginalCheckboxField ) => OriginalCheckboxField );
