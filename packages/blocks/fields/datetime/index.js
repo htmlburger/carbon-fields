8cb5705f1ea0c1e53f172e98e91cb236ca1e749c
Viktor Vasilev <liberalcho@gmail.com>
2018-11-21 11:40:29 +0200

    [DateTime] Polish styles

--- packages/blocks/fields/datetime/index.js
+++ packages/blocks/fields/datetime/index.js
@@ -4,4 +4,9 @@
 import { addFilter } from '@wordpress/hooks';
 import { __ } from '@wordpress/i18n';
 
+/**
+ * Internal dependencies.
+ */
+import './style.scss';
+
 addFilter( 'carbon-fields.date_time-field.block', 'carbon-fields/blocks', ( OriginalDatetimeField ) => ( props ) => ( <OriginalDatetimeField buttonText={ __( 'Select Date' ) } { ...props } /> ) );
