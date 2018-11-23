b9e7dfd971b68b7a2253f9686a9df85a0419163e
Viktor Vasilev <liberalcho@gmail.com>
2018-11-23 16:44:58 +0200

    [Set/Blocks] Implement validation

--- packages/blocks/fields/set/index.js
+++ packages/blocks/fields/set/index.js
@@ -7,5 +7,6 @@ import { addFilter } from '@wordpress/hooks';
  * Internal dependencies.
  */
 import './style.scss';
+import withValidation from '../../components/with-validation';
 
-addFilter( 'carbon-fields.set-field.block', 'carbon-fields/blocks', ( OriginalSetField ) => OriginalSetField );
+addFilter( 'carbon-fields.set-field.block', 'carbon-fields/blocks', ( OriginalSetField ) => withValidation( OriginalSetField ) );
