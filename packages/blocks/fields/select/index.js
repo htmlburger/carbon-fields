34877c85fba9ee33ef48f6f2dae5df8e28a582c3
Viktor Vasilev <liberalcho@gmail.com>
2018-11-23 17:04:28 +0200

    [Select/Blocks] Implement validation

--- packages/blocks/fields/select/index.js
+++ packages/blocks/fields/select/index.js
@@ -7,5 +7,6 @@ import { addFilter } from '@wordpress/hooks';
  * Internal dependencies.
  */
 import './style.scss';
+import withValidation from '../../components/with-validation';
 
-addFilter( 'carbon-fields.select-field.block', 'carbon-fields/blocks', ( OriginalSelectField ) => OriginalSelectField );
+addFilter( 'carbon-fields.select-field.block', 'carbon-fields/blocks', ( OriginalSelectField ) => withValidation( OriginalSelectField ) );
