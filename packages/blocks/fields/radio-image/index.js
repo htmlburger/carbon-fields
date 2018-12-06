809cbeb7998f9ae408adc148e7f910247a304762
jorostoyanov <stoyanov.gs@gmail.com>
2018-12-06 11:26:42 +0200

    Make use of withProps in radion image block

--- packages/blocks/fields/radio-image/index.js
+++ packages/blocks/fields/radio-image/index.js
@@ -2,23 +2,25 @@
  * External dependencies.
  */
 import { addFilter } from '@wordpress/hooks';
+import { compose } from '@wordpress/compose';
 
 /**
  * Internal dependencies.
  */
 import './style.scss';
+import { withProps } from '@carbon-fields/core';
 
-addFilter( 'carbon-fields.radio_image.block', 'carbon-fields/blocks', ( OriginalRadioField ) => ( props ) => {
-	return (
-		<OriginalRadioField
-			{ ...props }
-			field={ {
+addFilter( 'carbon-fields.radio_image.block', 'carbon-fields/blocks', compose(
+	withProps( ( props ) => {
+		return {
+			...props,
+			field: {
 				...props.field,
 				options: props.field.options.map( ( option ) => ( {
 					...option,
 					label: ( <img className="cf-radio-image__image" src={ option.label } /> )
 				} ) )
-			} }
-		/>
-	);
-} );
+			}
+		};
+	} )
+) );
