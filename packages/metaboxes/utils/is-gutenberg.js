e85f7ee4bea3f532799a118f1d2f53d85ce7c2e3
Viktor Vasilev <liberalcho@gmail.com>
2018-12-10 11:37:18 +0200

    [Metaboxes] Add comment about used variables in detection of Gutenberg

--- packages/metaboxes/utils/is-gutenberg.js
+++ packages/metaboxes/utils/is-gutenberg.js
@@ -6,6 +6,9 @@ import { isUndefined } from 'lodash';
 /**
  * Returns true if Gutenberg is presented.
  *
+ * For some reason the Gutenberg package uses `_wpLoadGutenbergEditor`
+ * while WordPress@5 uses `_wpLoadBlockEditor`.
+ *
  * @return {boolean}
  */
 export default function isGutenberg() {
