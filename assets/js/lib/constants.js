377979eca390d6511cc732f15cf0d2a7174621df
Viktor Vasilev <liberalcho@gmail.com>
2018-12-10 16:57:43 +0200

    Remove unused files

--- assets/js/lib/constants.js
+++
@@ -1,5 +0,0 @@
-export const PAGE_NOW_PAGE = 'page.php';
-export const PAGE_NOW_WIDGETS = 'widgets.php';
-export const PAGE_NOW_CUSTOMIZE = 'customize.php';
-export const PAGE_NOW_MENUS = 'nav-menus.php';
-export const KEY_ENTER = 13;
