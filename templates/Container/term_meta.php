2a8fc00f160a26952bd5c3ef20a328968f57498e
Viktor Vasilev <liberalcho@gmail.com>
2018-12-17 11:33:55 +0200

    [Metaboxes] Fix layout issues of term meta container

--- templates/Container/term_meta.php
+++ templates/Container/term_meta.php
@@ -1,8 +1,16 @@
-<tr class="carbon-table-row">
-	<td></td>
-	<td>
-		<div id="<?php echo $this->get_id(); ?>">
-			<fieldset class="<?php echo ! empty( $_GET['tag_ID'] ) ? 'edit-term-container' : 'add-term-container'; ?> container-<?php echo $this->get_id(); ?>"></fieldset>
-		</div>
-	</td>
-</tr>
+<?php ob_start(); ?>
+	<fieldset class="container-<?php echo $this->get_id(); ?>"></fieldset>
+<?php $html = ob_get_clean(); ?>
+
+<?php if ( ! empty( $_GET['tag_ID'] ) ): ?>
+	<tr class="carbon-table-row">
+		<td></td>
+		<td>
+			<?php echo $html; ?>
+		</td>
+	</tr>
+<?php else: ?>
+	<div class="form-field">
+		<?php echo $html; ?>
+	</div>
+<?php endif; ?>
