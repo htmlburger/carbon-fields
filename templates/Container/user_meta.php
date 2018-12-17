73b5456483dad14082d67978baa3725bf5116a28
Viktor Vasilev <liberalcho@gmail.com>
2018-12-17 12:24:16 +0200

    [Metaboxes] Fix layout issues of user meta container

--- templates/Container/user_meta.php
+++ templates/Container/user_meta.php
@@ -1,11 +1,12 @@
-<div id="<?php echo $this->get_id(); ?>">
-	<h3><?php echo $this->title; ?></h3>
-	<table class="form-table">
-		<tr class="carbon-table-row">
+<h2><?php echo $this->title; ?></h2>
+
+<table class="form-table">
+	<tbody>
+		<tr>
 			<th></th>
 			<td>
 				<fieldset class="container-<?php echo $this->get_id(); ?>" data-profile-role="<?php echo $profile_role ?>"></fieldset>
 			</td>
 		</tr>
-	</table>
-</div>
+	</tbody>
+</table>
