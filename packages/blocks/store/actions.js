1095bffa7f41b9704b7235395138bcae9133e003
Viktor Vasilev <liberalcho@gmail.com>
2018-12-19 11:40:03 +0200

    [Blocks] Add support for tabbed layout

--- packages/blocks/store/actions.js
+++ packages/blocks/store/actions.js
@@ -4,6 +4,21 @@
  * @param  {Object} definitions
  * @return {Object}
  */
+export function setupContainerDefinitions( definitions ) {
+	return {
+		type: 'SETUP_CONTAINER_DEFINITIONS',
+		payload: {
+			definitions
+		}
+	};
+}
+
+/**
+ * Returns an action object used to setup the definitions state when first opening an editor.
+ *
+ * @param  {Object} definitions
+ * @return {Object}
+ */
 export function setupFieldDefinitions( definitions ) {
 	return {
 		type: 'SETUP_FIELD_DEFINITIONS',
