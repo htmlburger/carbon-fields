cb590087984a4cb05ff3b46f7ef1a3b40fbad67a
lstoyanoff <lstoyanov.2create@gmail.com>
2020-02-03 17:14:40 +0200

    Fix select field value when rendered

--- packages/core/fields/select/index.js
+++ packages/core/fields/select/index.js
@@ -32,9 +32,13 @@ export class SelectField extends Component {
 		const {
 			id,
 			name,
-			value,
-			field
+			field,
+			onChange
 		} = this.props;
+
+		const value = this.props.value || get( field.options, '[0].value', '' );
+
+		onChange( id, value );
 
 		return (
 			field.options.length > 0
@@ -42,7 +46,7 @@ export class SelectField extends Component {
 					<select
 						id={ id }
 						name={ name }
-						value={ value ? value : get( field.options, '[0].value', '' ) }
+						value={ value }
 						className="cf-select__input"
 						onChange={ this.handleChange }
 					>
