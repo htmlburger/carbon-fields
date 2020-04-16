e690755bf8e7dee3aa0e5e9bf20c1f3d24261002
lstoyanoff <lstoyanov.2create@gmail.com>
2020-04-16 12:23:14 +0300

    Bump v3.1.17

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ namespace Carbon_Fields;
 
 # Define version constant
 if ( ! defined( __NAMESPACE__ . '\VERSION' ) ) {
-	define( __NAMESPACE__ . '\VERSION', '3.1.16' );
+	define( __NAMESPACE__ . '\VERSION', '3.1.17' );
 }
 
 # Define root directory
