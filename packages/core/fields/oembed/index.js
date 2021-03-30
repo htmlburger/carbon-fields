e676bd7e347fd75c86d59468496f3c27712165e4
Joro Stoyanov <stoyanov.gs@gmail.com>
2021-03-30 11:35:30 +0300

    Use global jQuery in oembed field

--- packages/core/fields/oembed/index.js
+++ packages/core/fields/oembed/index.js
@@ -15,7 +15,6 @@ import {
 	isEmpty,
 	debounce
 } from 'lodash';
-import $ from 'jquery';
 
 /**
  * The internal dependencies.
@@ -179,7 +178,7 @@ function handler( props ) {
 
 		switch ( type ) {
 			case 'FETCH_EMBED_CODE':
-				const request = $.get( window.wpApiSettings.root + 'oembed/1.0/proxy', {
+				const request = window.jQuery.get( window.wpApiSettings.root + 'oembed/1.0/proxy', {
 					url: payload,
 					_wpnonce: window.wpApiSettings.nonce
 				} );
