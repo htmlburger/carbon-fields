f41f1b91b1fd6de4f495b9ba3e87373514f284e6
gettonet <59503945+gettonet@users.noreply.github.com>
2022-11-08 09:35:31 +0100

    Add files via upload

--- core/Pimple/ServiceIterator.php
+++ core/Pimple/ServiceIterator.php
@@ -27,18 +27,63 @@
 namespace Carbon_Fields\Pimple;
 
 /**
- * Pimple service provider interface.
+ * Lazy service iterator.
  *
- * @author  Fabien Potencier
- * @author  Dominik Zogg
+ * @author Pascal Luna <skalpa@zetareticuli.org>
  */
-interface ServiceProviderInterface
+final class ServiceIterator implements \Iterator
 {
+    private $container;
+    private $ids;
+
+    public function __construct(Container $container, array $ids)
+    {
+        $this->container = $container;
+        $this->ids = $ids;
+    }
+
     /**
-     * Registers services on the given container.
-     *
-     * This method should only be used to configure services and parameters.
-     * It should not get services.
+     * @return void
      */
-    public function register(Container $pimple);
+    #[\ReturnTypeWillChange]
+    public function rewind()
+    {
+        \reset($this->ids);
+    }
+
+    /**
+     * @return mixed
+     */
+    #[\ReturnTypeWillChange]
+    public function current()
+    {
+        return $this->container[\current($this->ids)];
+    }
+
+    /**
+     * @return mixed
+     */
+    #[\ReturnTypeWillChange]
+    public function key()
+    {
+        return \current($this->ids);
+    }
+
+    /**
+     * @return void
+     */
+    #[\ReturnTypeWillChange]
+    public function next()
+    {
+        \next($this->ids);
+    }
+
+    /**
+     * @return bool
+     */
+    #[\ReturnTypeWillChange]
+    public function valid()
+    {
+        return null !== \key($this->ids);
+    }
 }
