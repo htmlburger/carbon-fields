4b6093ef04dbd543ebf068f59edc926409300012
HTMLBurger-NG <ngenchev+carbons@2create.studio>
2023-02-22 10:45:04 +0200

    Allow inputmode attribute for Text_Field

--- core/Field/Text_Field.php
+++ core/Field/Text_Field.php
@@ -12,5 +12,5 @@ class Text_Field extends Field {
 	/**
 	 * {@inheritDoc}
 	 */
-	protected $allowed_attributes = array( 'list', 'max', 'maxLength', 'min', 'pattern', 'placeholder', 'readOnly', 'step', 'type', 'is' );
+	protected $allowed_attributes = array( 'list', 'max', 'maxLength', 'min', 'pattern', 'placeholder', 'readOnly', 'step', 'type', 'is', 'inputmode' );
 }
