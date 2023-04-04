47f1538cca6cd2860d1b37a040a42c59eb798d76
HTMLBurger-NG <ngenchev+carbons@2create.studio>
2023-04-04 11:41:31 +0300

    Bump v3.6.0

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ namespace Carbon_Fields;
 
 # Define version constant
 if ( ! defined( __NAMESPACE__ . '\VERSION' ) ) {
-	define( __NAMESPACE__ . '\VERSION', '3.5.2' );
+	define( __NAMESPACE__ . '\VERSION', '3.6.0' );
 }
 
 # Define root directory
